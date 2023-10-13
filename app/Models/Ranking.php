347233285bd84ce6f32fea66692be886bed08f53
danisec <daniaprilyanto35@gmail.com>
2023-10-13 18:35:32 +0700

    add: models/Ranking

---
+++ app/Models/Ranking.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Ranking extends Model
+{
+    use HasFactory;
+
+    protected $table = 'ranking';
+    protected $guarded = ['id_ranking'];
+
+    public function alternatif()
+    {
+        return $this->belongsTo(Alternatif::class, 'kode_alternatif', 'kode_alternatif');
+    }
+}
