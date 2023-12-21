e7d0ad31febda017cc8ef7f2df2c6ee663d3511b
danisec <daniaprilyanto35@gmail.com>
2023-12-21 21:30:26 +0700

    update: js/app.js

--- resources/js/app.js
+++ resources/js/app.js
@@ -2,6 +2,9 @@ import "./bootstrap";
 import jQuery from "jquery";
 import Alpine from "alpinejs";
 import focus from "@alpinejs/focus";
+import "./subkriteria/app";
+import "./skalaIndikator/app";
+import "./textArea/textAreaHeight";
 
 window.$ = jQuery;
 
