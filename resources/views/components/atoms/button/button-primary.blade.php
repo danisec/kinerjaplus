636a6dbf0aabf57119db43b64e42f9c2684ba12d
danisec <daniaprilyanto35@gmail.com>
2023-12-21 22:03:09 +0700

    refactor: button-primary

--- resources/views/components/atoms/button/button-primary.blade.php
+++ resources/views/components/atoms/button/button-primary.blade.php
@@ -2,5 +2,5 @@
 
 <button
     class="{{ $customClass }} bg-indigo-600 text-base font-medium text-white hover:bg-indigo-700 focus:outline-none focus:ring-4 focus:ring-indigo-300"
-    type="{{ $type }}">{{ $name }}
+    type="{{ $type }}" {{ $attributes }}>{{ $name }}
 </button>
