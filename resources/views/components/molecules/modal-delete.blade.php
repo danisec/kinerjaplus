7c677e33ff2375a3118fde57f653f07d4aa336fc
danisec <daniaprilyanto35@gmail.com>
2023-12-21 22:04:18 +0700

    update: components/modal-delete

--- resources/views/components/molecules/modal-delete.blade.php
+++ resources/views/components/molecules/modal-delete.blade.php
@@ -4,13 +4,14 @@
     x-transition:enter="transition ease-out duration-100" x-transition:enter-start="transform opacity-0 scale-95"
     x-transition:enter-end="transform opacity-100 scale-100" x-cloak>
 
-    <div class="rounded-lg bg-white p-12 shadow-lg">
+    <div class="w-7/12 rounded-lg bg-white p-12 shadow-lg">
         <h2 class="mb-1 text-2xl font-semibold">Konfirmasi</h2>
 
         <p class="text-base font-normal text-gray-900">{{ $title }}</p>
 
         <div class="mt-8 flex justify-start gap-4">
-            <button class="rounded-md border border-gray-300 px-6 py-2.5 text-gray-900" @click="isOpen = false">
+            <button class="rounded-md border border-gray-300 px-6 py-2.5 text-gray-900" type="button"
+                @click="isOpen = false">
                 Batal
             </button>
 
@@ -19,7 +20,8 @@
                 @csrf
 
                 <button
-                    class="rounded-md bg-red-500 px-6 py-2.5 text-white hover:bg-red-600 focus:bg-red-600 focus:ring-red-600">
+                    class="rounded-md bg-red-500 px-6 py-2.5 text-white hover:bg-red-600 focus:bg-red-600 focus:ring-red-600"
+                    type="submit">
                     Hapus
                 </button>
             </form>
