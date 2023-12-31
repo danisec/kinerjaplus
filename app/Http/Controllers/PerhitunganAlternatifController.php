daded11f4e95b39b2ed18f7003ef5d2d80d78240
danisec <daniaprilyanto35@gmail.com>
2023-12-31 16:37:33 +0700

    update: PerhitunganAlternatifController

--- app/Http/Controllers/PerhitunganAlternatifController.php
+++ app/Http/Controllers/PerhitunganAlternatifController.php
@@ -6,6 +6,7 @@ use App\Models\Alternatif;
 use App\Models\BobotPrioritasAlternatif;
 use App\Models\Kriteria;
 use App\Models\PerhitunganAlternatif;
+use App\Services\PerhitunganAlternatifService;
 use Illuminate\Http\Request;
 
 class PerhitunganAlternatifController extends Controller
@@ -16,12 +17,14 @@ class PerhitunganAlternatifController extends Controller
     private $kriteria;
     private $alternatif;
     private $perhitunganAlternatif;
+    private $perhitunganAlternatifService;
 
-    public function __construct()
+    public function __construct(PerhitunganAlternatifService $perhitunganAlternatifService)
     {
         $this->kriteria = Kriteria::orderBy('kode_kriteria', 'asc')->get();
         $this->alternatif = Alternatif::orderBy('kode_alternatif', 'asc')->get();
         $this->perhitunganAlternatif = PerhitunganAlternatif::with('alternatifPertama', 'alternatifKedua')->orderBy('alternatif_pertama', 'asc')->get();
+        $this->perhitunganAlternatifService = $perhitunganAlternatifService;
     }
 
     /**
@@ -57,13 +60,11 @@ class PerhitunganAlternatifController extends Controller
         $matriks = $validatedData['matriks'];
 
         try {
-            // Clear the existing records
             PerhitunganAlternatif::truncate();
 
             foreach ($matriks as $kriteria => $dataKriteria) {
                 foreach ($dataKriteria as $alternatifPertama => $dataAlternatif) {
                     foreach ($dataAlternatif as $alternatifKedua => $nilaiAlternatif) {
-                        // Create or update the record
                         PerhitunganAlternatif::updateOrCreate(
                             [
                                 'kode_kriteria' => $kriteria,
@@ -76,8 +77,7 @@ class PerhitunganAlternatifController extends Controller
                 }
             }
 
-            $notif = notify()->success('Data perbandingan alternatif berhasil disimpan');
-            return redirect()->route('perhitunganAlternatif.hasil')->with('notif', $notif);
+            return redirect()->route('perhitunganAlternatif.hasil')->withInput();
         } catch (\Throwable $th) {
             $notif = notify()->error('Data perbandingan alternatif gagal disimpan');
             return redirect()->route('perhitunganAlternatif.index')->with('notif', $notif);
@@ -93,104 +93,20 @@ class PerhitunganAlternatifController extends Controller
         $alternatif = $this->alternatif;
         $perhitunganAlternatif = $this->perhitunganAlternatif;
 
-        /* Menghitung total per kolom alternatif  */
-        $columnTotals = [];
-        foreach ($perhitunganAlternatif as $item) {
-            $kodeKriteria = $item->kode_kriteria;
-            $alternatifKedua = $item->alternatif_kedua;
-            $nilaiAlternatif = $item->nilai_alternatif;
+        $totalKolomAlternatif = $this->perhitunganAlternatifService->totalKolomAlternatif($perhitunganAlternatif);
+        $normalisasiMatriks = $this->perhitunganAlternatifService->normalisasiMatriks($perhitunganAlternatif, $totalKolomAlternatif);
 
-            if (!isset($columnTotals[$kodeKriteria][$alternatifKedua])) {
-                $columnTotals[$kodeKriteria][$alternatifKedua] = 0;
-            }
-
-            $columnTotals[$kodeKriteria][$alternatifKedua] += $nilaiAlternatif;
-
-            // Urutkan kriteria secara ascending
-            ksort($columnTotals);
-        }
-
-        /* Normalisasi matriks alternatif
-           Rumus = nilai alternatif / total kolom
-         */
-        $normalizedMatrix = [];
-        foreach ($perhitunganAlternatif as $item) {
-            $kodeKriteria = $item->kode_kriteria;
-            $alternatifPertama = $item->alternatif_pertama;
-            $alternatifKedua = $item->alternatif_kedua;
-            $nilaiAlternatif = $item->nilai_alternatif;
-
-            if (!isset($normalizedMatrix[$kodeKriteria][$alternatifPertama][$alternatifKedua])) {
-                $normalizedMatrix[$kodeKriteria][$alternatifPertama][$alternatifKedua] = 0;
-            }
-
-            $normalizedMatrix[$kodeKriteria][$alternatifPertama][$alternatifKedua] = $nilaiAlternatif / $columnTotals[$kodeKriteria][$alternatifKedua];
-
-            // Dapatkan 4 angka di belakang koma
-            $normalizedMatrix[$kodeKriteria][$alternatifPertama][$alternatifKedua] = substr($normalizedMatrix[$kodeKriteria][$alternatifPertama][$alternatifKedua], 0, 6);
-        }
-
-        /* Menjumlahkan nilai per baris alternatif dari matriks normalisasi */
-        $rowTotals = [];
-        foreach ($normalizedMatrix as $kodeKriteria => $matrixKriteria) {
-            foreach ($matrixKriteria as $alternatifPertama => $matrixAlternatif) {
-                if (!isset($rowTotals[$kodeKriteria][$alternatifPertama])) {
-                    $rowTotals[$kodeKriteria][$alternatifPertama] = 0;
-                }
-
-                $rowTotals[$kodeKriteria][$alternatifPertama] = array_sum($matrixAlternatif);
-
-                // Dapatkan 4 angka di belakang koma
-                $rowTotals[$kodeKriteria][$alternatifPertama] = substr($rowTotals[$kodeKriteria][$alternatifPertama], 0, 6);
-            }
-        }
-
-        // Menghitung jumlah alternatif
-        $jumlahAlternatif = Alternatif::count();
-
-        /* Menghitung Bobot
-           Rumus = total baris / jumlah alternatif
-         */
-        $bobot = [];
-        foreach ($rowTotals as $kodeKriteria => $matrixKriteria) {
-            foreach ($matrixKriteria as $alternatifPertama => $total) {
-                $bobot[$kodeKriteria][$alternatifPertama] = $total / $jumlahAlternatif;
-
-                // Dapatkan 4 angka di belakang koma
-                $bobot[$kodeKriteria][$alternatifPertama] = substr($bobot[$kodeKriteria][$alternatifPertama], 0, 6);
-            }
-        }
-
-        // Simpan atau ubah data bobot prioritas alternatif ke database
-        try {
-            // Clear the existing records
-            BobotPrioritasAlternatif::truncate();
-
-            foreach ($bobot as $kodeKriteria => $matrixKriteria) {
-                foreach ($matrixKriteria as $dataAlternatif => $bobotPrioritas) {
-                    // Create or update the record
-                    BobotPrioritasAlternatif::updateOrCreate(
-                        [
-                            'kode_kriteria' => $kodeKriteria,
-                            'kode_alternatif' => $dataAlternatif,
-                        ],
-                        ['bobot_prioritas' => $bobotPrioritas]
-                    );
-                }
-            }
-        } catch (\Throwable $th) {
-            $notif = notify()->error('Terjadi kesalahan saat menyimpan data bobot prioritas alternatif');
-            return back()->with('notif', $notif);
-        }
+        $totalBarisNormalisasiMatriks = $this->perhitunganAlternatifService->totalBarisNormalisasiMatriks($normalisasiMatriks);
+        $bobotPrioritasAlternatif = $this->perhitunganAlternatifService->bobotPrioritasAlternatif($totalBarisNormalisasiMatriks);
 
         return view('pages.dashboard.perhitungan-alternatif.hasil', [
             'title' => 'Hasil Perhitungan Alternatif',
             'kriteria' => $kriteria,
             'alternatif' => $alternatif,
             'perhitunganAlternatif' => $perhitunganAlternatif,
-            'columnTotals' => $columnTotals,
-            'normalizedMatrix' => $normalizedMatrix,
-            'bobot' => $bobot,
+            'totalKolomAlternatif' => $totalKolomAlternatif,
+            'normalisasiMatriks' => $normalisasiMatriks,
+            'bobotPrioritasAlternatif' => $bobotPrioritasAlternatif,
         ]);
     }
 
