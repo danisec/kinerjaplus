f3103a125479000100c455df828edd6a79fab7e0
danisec <daniaprilyanto35@gmail.com>
2023-12-31 16:47:59 +0700

    add: NilaiSkalaSeeder

---
+++ database/seeders/NilaiSkalaSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class NilaiSkalaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
