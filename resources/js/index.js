05c0e645ff50fa47b41062fd4dbb207071bf9b16
danisec <daniaprilyanto35@gmail.com>
2023-12-31 16:23:00 +0700

    add: js/index

---
+++ resources/js/index.js
@@ -0,0 +1,6 @@
+import "./subkriteria/app";
+import "./skalaIndikator/app";
+import "./textArea/app";
+import "./perhitunganKriteria/app";
+import "./perhitunganSubkriteria/app";
+import "./perhitunganAlternatif/app";
