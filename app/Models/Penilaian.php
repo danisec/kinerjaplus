93593d12d5eed0e7d8c4c28d4ef43a02450c51ca
danisec <daniaprilyanto35@gmail.com>
2023-12-31 16:40:18 +0700

    update: models/Penilaian

--- app/Models/Penilaian.php
+++ app/Models/Penilaian.php
@@ -8,4 +8,39 @@ use Illuminate\Database\Eloquent\Model;
 class Penilaian extends Model
 {
     use HasFactory;
+
+    protected $table = 'penilaian';
+    protected $guarded = ['id_penilaian'];
+
+    public function scopeFilter($query, array $filters)
+    {
+        $query->when($filters['search'] ?? false, function ($query, $search) {
+            $query->where(function ($query) use ($search) {
+                $query->whereHas('alternatifPertama', function ($query) use ($search) {
+                    $query->where('nama_alternatif', 'like', '%' . $search . '%');
+                });
+                $query->orWhere('tahun_ajaran', 'like', '%'. $search . '%');
+            });
+        });
+    }
+
+    public function penilaianIndikator()
+    {
+        return $this->hasMany(PenilaianIndikator::class, 'id_penilaian', 'id_penilaian');
+    }
+
+    public function alternatifPertama()
+    {
+        return $this->belongsTo(Alternatif::class, 'alternatif_pertama', 'kode_alternatif');
+    }
+
+    public function alternatifKedua()
+    {
+        return $this->belongsTo(Alternatif::class, 'alternatif_kedua', 'kode_alternatif');
+    }
+
+    public function kriteria()
+    {
+        return $this->belongsTo(Kriteria::class, 'kode_kriteria', 'kode_kriteria');
+    }
 }
