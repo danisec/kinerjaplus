a499ceb5f5ea66fe856eef03b3c3173465e6aec9
danisec <daniaprilyanto35@gmail.com>
2024-04-04 13:43:25 +0700

    update: js/textArea

--- resources/js/textArea/app.js
+++ resources/js/textArea/app.js
@@ -1,7 +1,8 @@
 if (
     window.location.pathname.includes("/dashboard/data-subkriteria/") ||
     window.location.pathname.includes("/dashboard/data-skala-indikator/") ||
-    window.location.pathname.includes("/dashboard/data-penilaian/")
+    window.location.pathname.includes("/dashboard/data-penilaian/") ||
+    window.location.pathname.includes("/dashboard/catatan-karyawan/")
 ) {
     import("./textAreaHeight")
         .then((module) => {})
