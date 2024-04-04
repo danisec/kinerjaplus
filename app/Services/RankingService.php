a8cc5a015612c7ca13ad13e83d6058641f39386e
danisec <daniaprilyanto35@gmail.com>
2024-04-05 00:48:34 +0700

    add: RankingService

---
+++ app/Services/RankingService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Http\Request;
+
+class RankingService
+{
+    public function totalBobotKriteria($uniqueAlternatifPenilaianByTahunAjaran, $kriteria, $bobotPrioritasSubkriteria, $bobotAlternatif)
+    {
+        $totalBobotKriteria = [];
+        foreach ($uniqueAlternatifPenilaianByTahunAjaran as $alternatifItem) {
+            $total = 0;
+
+            foreach ($kriteria as $kriteriaItem) {
+                $bobotPrioritasSubkriteriaItem = $bobotPrioritasSubkriteria->where('kriteria.kode_kriteria', $kriteriaItem->kode_kriteria)->first();
+
+                $bobotPrioritasAlternatifItem = $bobotAlternatif->where('kode_kriteria', $kriteriaItem->kode_kriteria)->where('kode_alternatif', $alternatifItem->alternatifKedua->kode_alternatif)->first();
+
+                if ($bobotPrioritasSubkriteriaItem && $bobotPrioritasAlternatifItem) {
+                    $total += ($bobotPrioritasSubkriteriaItem->bobot_prioritas * $bobotPrioritasAlternatifItem->bobot_prioritas);
+                }
+            }
+
+            $totalBobotKriteria[$alternatifItem->alternatifKedua->kode_alternatif] = $total;
+            $totalBobotKriteria[$alternatifItem->alternatifKedua->kode_alternatif] = substr($total, 0, 6);
+        }
+
+        return $totalBobotKriteria;
+    }
+}
