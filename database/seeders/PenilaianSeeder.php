8097ce8cfc476b4aedb876a08f65460c2a878171
danisec <daniaprilyanto35@gmail.com>
2024-04-04 13:25:50 +0700

    update: PenilaianSeeder

--- database/seeders/PenilaianSeeder.php
+++ database/seeders/PenilaianSeeder.php
@@ -12,6 +12,23 @@ class PenilaianSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $alternatif = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
+
+        $penilaian = [];
+
+        foreach ($alternatif as $alternatifPertama) {
+            foreach ($alternatif as $alternatifKedua) {
+                $penilaian[] = [
+                    'tahun_ajaran' => '2023/2024',
+                    'alternatif_pertama' => $alternatifPertama,
+                    'alternatif_kedua' => $alternatifKedua,
+                    'status' => 'Disetujui',                    
+                    'created_at' => now(),
+                    'updated_at' => now(),
+                ];
+            }
+        }
+
+        \App\Models\Penilaian::insert($penilaian);
     }
 }
