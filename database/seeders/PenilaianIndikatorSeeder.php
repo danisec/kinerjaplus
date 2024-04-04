5ce3e628184fa29d98014cf23f2b08b098f440c1
danisec <daniaprilyanto35@gmail.com>
2024-04-05 00:50:20 +0700

    add: PenilaianIndikatorSeeder

---
+++ database/seeders/PenilaianIndikatorSeeder.php
@@ -0,0 +1,202 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class PenilaianIndikatorSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $penilaianIndikator = [
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(1, 4),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(5, 8),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(9, 12),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(13, 16),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(17, 20),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(21, 24),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(25, 28),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(29, 32),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(33, 36),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(37, 40),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(41, 44),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(45, 48),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(49, 52),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(53, 56),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(57, 60),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(61, 64),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(65, 68),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(69, 72),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(73, 76),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(77, 80),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(81, 84),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(85, 88),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(89, 92),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(93, 96),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(97, 100),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(101, 104),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(105, 108),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(109, 112),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(113, 116),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'id_penilaian' => '64',
+                'id_skala_indikator_detail' => rand(117, 120),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($penilaianIndikator as $data) {
+            \App\Models\PenilaianIndikator::create($data);
+        }
+    }
+}
