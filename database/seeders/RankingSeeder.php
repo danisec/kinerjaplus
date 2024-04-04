320a4d6e9eb6716837e859d39ecb9bee113da19e
danisec <daniaprilyanto35@gmail.com>
2024-04-04 13:26:05 +0700

    update: RankingSeeder

--- database/seeders/RankingSeeder.php
+++ database/seeders/RankingSeeder.php
@@ -2,8 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Ranking;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class RankingSeeder extends Seeder
 {
@@ -12,6 +14,19 @@ class RankingSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $kodeAlternatif = ['A1', 'A2', 'A3', 'A4', 'A5', 'A6', 'A7', 'A8'];
+
+        foreach ($kodeAlternatif as $kode) {
+            $ranking = [
+                'tahun_ajaran' => '2024/2025',
+                'kode_alternatif' => $kode,
+                'nilai' => rand(60, 100),
+                'rank' => rand(1, 8),
+                'created_at' => now(),
+                'updated_at' => now(),
+            ];
+
+            DB::table('ranking')->insert($ranking);
+        }
     }
 }
