dedb92ea78c15a2147935b4097d9dc006790da6f
danisec <daniaprilyanto35@gmail.com>
2024-04-05 00:49:57 +0700

    add: IndikatorSubkriteriaSeeder

---
+++ database/seeders/IndikatorSubkriteriaSeeder.php
@@ -0,0 +1,204 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class IndikatorSubkriteriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $indikatorSubkriteria = [
+            [
+                'kode_subkriteria' => 'SK1.1',
+                'indikator_subkriteria' => 'Menjalankan nilai-nilai Alkitab.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.1',
+                'indikator_subkriteria' => 'Melakukan tanggung jawab selaras dengan etika kerja dan aturan instansi.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.1',
+                'indikator_subkriteria' => 'Menghadapi dan menyelesaikan konflik dengan tepat.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.2',
+                'indikator_subkriteria' => 'Melaksanakan tugas dengan percaya diri dan penuh tanggung jawab.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.2',
+                'indikator_subkriteria' => 'Mampu membagikan visi instansi kepada orang lain.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.2',
+                'indikator_subkriteria' => 'Memberikan pertolongan bagi orang lain sesuai kemampuan yang dimiliki.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.2',
+                'indikator_subkriteria' => 'Memberdayakan orang lain.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.3',
+                'indikator_subkriteria' => 'Menghargai dan memperlakukan orang lain secara terhormat.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.3',
+                'indikator_subkriteria' => 'Bekerja sama dengan orang lain, saling membantu satu sama lainnya dalam mewujudkan tujuan bersama.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK1.3',
+                'indikator_subkriteria' => 'Memelihara ciptaan Tuhan, menjaga dan melestarikan lingkungan.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'kode_subkriteria' => 'SK2.1',
+                'indikator_subkriteria' => 'Guru menyiapkan fisik dan psikis murid dengan menyapa dan memberi salam.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.2',
+                'indikator_subkriteria' => 'Guru mengajukan pertanyaan pemantik yang menantang untuk memotivasi murid.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.3',
+                'indikator_subkriteria' => 'Guru menyampaikan capaian pembelajaran dan tujuan pembelajaran yang akan dicapai.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.4',
+                'indikator_subkriteria' => 'Guru menyampaikan topik materi ajar untuk mengarahkan siswa menjawab pertanyaan pemantik sesuai dengan tujuan pembelajaran.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.5',
+                'indikator_subkriteria' => 'Guru melaksanakan pembelajaran yang kontekstual untuk menumbuhkan dan mengembangkan keterampilan, kebiasaan dan sikap positif sesuai dengan alokasi waktu yang direncanakan.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.5',
+                'indikator_subkriteria' => 'Guru melaksanakan pembelajaran yang menumbuhkan partisipasi aktif murid dalam mengajukan pertanyaan.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.6',
+                'indikator_subkriteria' => 'Melaksanakan pembelajaran yang mengasah kemampuan Creativity, Critical, Communication, Collaboration thinking (menumbuhkan KSE Murid).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Menciptakan suasana kelas yang kondusif untuk proses belajar mengajar (sesuai dengan kesepakatan kelas dan KSE).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Mengomunikasikan pesan bahwa guru percaya akan kemampuan semua murid untuk belajar dan berprestasi secara akademik.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Melaksanakan praktik adaptasi pengajaran sebagai respon atas umpan balik dan respon murid terhadap kebutuhan belajarnya, dapat memberikan konten materi berbeda dalam capaian pembelajaran yang sama di beberapa kelompok (Diferensiasi Konten).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Menunjukkan keterampilan dalam penggunaan sumber belajar yang bervariasi dalam bentuk visual, Audio dan kinestetik (Diferensiasi Proses).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Menunjukkan keterampilan dalam penggunaan media pembelajaran.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.7',
+                'indikator_subkriteria' => 'Guru dapat menggunakan bahasa lisan secara jelas dan dapat menggunakan bahasa tulis yang baik dan benar (komunikasi efektif).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.8',
+                'indikator_subkriteria' => 'Melakukan pengajaran yang mendorong keterampilan literasi dan numerasi murid.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK2.9',
+                'indikator_subkriteria' => 'Memfasilitasi dan membimbing murid merangkum materi pelajaran. (refleksi)',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+
+            [
+                'kode_subkriteria' => 'SK3.1',
+                'indikator_subkriteria' => 'Melaksanakan penilaian produk melalui proyek / hasil produk (Diferensiasi Produk).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK3.2',
+                'indikator_subkriteria' => 'RPP dan berkas administrasi yang lengkap dan memenuhi aspek sesuai ketentuan.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK3.2',
+                'indikator_subkriteria' => 'Ketepatan waktu kehadiran dan penyerahan berkas.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK3.2',
+                'indikator_subkriteria' => 'Keikutsertaan dalam kegiatan wajib di luar jam kerja.',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kode_subkriteria' => 'SK3.2',
+                'indikator_subkriteria' => 'Kehadiran (presensi).',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($indikatorSubkriteria as $data) {
+            \App\Models\IndikatorSubkriteria::create($data);
+        }
+    }
+}
