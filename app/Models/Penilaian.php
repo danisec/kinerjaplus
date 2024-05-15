dcfd6001538abe145d3a12c48c1ea1eb85494d49
danisec <daniaprilyanto35@gmail.com>
2024-05-15 14:46:57 +0700

    update: models/Penilaian

--- app/Models/Penilaian.php
+++ app/Models/Penilaian.php
@@ -11,14 +11,15 @@ class Penilaian extends Model
 
     protected $table = 'penilaian';
     protected $guarded = ['id_penilaian'];
+    protected $primaryKey = 'id_penilaian';
 
     public function scopeFilter($query, array $filters)
     {
         $query->when($filters['search'] ?? false, function ($query, $search) {
             $query->where(function ($query) use ($search) {
-                $query->whereHas('alternatifPertama', function ($query) use ($search) {
+                $query->whereHas('alternatifPertama.alternatifPertama', function ($query) use ($search) {
                     $query->where('nama_alternatif', 'like', '%' . $search . '%');
-                })->orWhereHas('alternatifKedua', function ($query) use ($search) {
+                })->orWhereHas('alternatifKedua.alternatifPertama', function ($query) use ($search) {
                     $query->where('nama_alternatif', 'like', '%' . $search . '%');
                 })->orWhere('tahun_ajaran', 'like', '%' . $search . '%');
             });
@@ -30,14 +31,19 @@ class Penilaian extends Model
         return $this->hasMany(PenilaianIndikator::class, 'id_penilaian', 'id_penilaian');
     }
 
+    public function catatanKaryawan()
+    {
+        return $this->belongsTo(CatatanKaryawan::class, 'id_penilaian', 'id_penilaian');
+    }
+
     public function alternatifPertama()
     {
-        return $this->belongsTo(Alternatif::class, 'alternatif_pertama', 'kode_alternatif');
+        return $this->belongsTo(GroupPenilaian::class, 'alternatif_pertama', 'alternatif_pertama');
     }
 
     public function alternatifKedua()
     {
-        return $this->belongsTo(Alternatif::class, 'alternatif_kedua', 'kode_alternatif');
+        return $this->belongsTo(GroupPenilaian::class, 'alternatif_kedua', 'alternatif_pertama');
     }
 
     public function kriteria()
