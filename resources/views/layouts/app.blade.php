5b6762c60fecb2a949b4215d0d4e29a67bf8e1f2
danisec <daniaprilyanto35@gmail.com>
2024-05-15 15:00:03 +0700

    update: layouts/app

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,20 +12,31 @@
     <title>{{ $title }} - {{ config('app.name') }}</title>
 
     @notifyCss
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    @vite(['resources/css/app.css', 'resources/css/multi-select-tag.css', 'resources/js/app.js'])
 </head>
 
 <body>
     @include('notify::components.notify')
 
-    @if (Auth::check())
-        <div>
-            {{ $slot }}
-        </div>
+    @php
+        $agent = new \Jenssegers\Agent\Agent();
+    @endphp
+
+    @if ($agent->isDesktop())
+        @if (Auth::check())
+            <div>
+                {{ $slot }}
+            </div>
+        @else
+            <main>
+                {{ $slot }}
+            </main>
+        @endif
     @else
-        <main>
-            {{ $slot }}
-        </main>
+        <div class="mx-auto flex min-h-screen flex-col justify-center p-4">
+            <p class="text-base font-medium text-gray-900">Maaf, web aplikasi KinerjaPlus hanya dapat diakses melalui
+                tampilan desktop.</p>
+        </div>
     @endif
 
     @notifyJs
