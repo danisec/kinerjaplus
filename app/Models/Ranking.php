71ef16e0a06ad0132b5a05dfc995eecfcd5d6228
danisec <daniaprilyanto35@gmail.com>
2024-05-15 14:47:25 +0700

    update: models/Ranking

--- app/Models/Ranking.php
+++ app/Models/Ranking.php
@@ -4,17 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Kyslik\ColumnSortable\Sortable;
 
 class Ranking extends Model
 {
-    use HasFactory;
+    use HasFactory, Sortable;
 
     protected $table = 'ranking';
     protected $guarded = ['id_ranking'];
     protected $primaryKey = 'id_ranking';
 
+    public $sortable = [
+        'tahun_ajaran',
+        'kode_alternatif',
+        'nilai',
+        'rank',
+    ];
+
     public function alternatif()
     {
-        return $this->belongsTo(Alternatif::class, 'kode_alternatif', 'kode_alternatif');
+        return $this->belongsTo(GroupPenilaian::class, 'kode_alternatif', 'alternatif_pertama');
     }
 }
