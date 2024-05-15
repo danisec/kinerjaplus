e9485fcd367ac52384afcb646d3e969de44e5f89
danisec <daniaprilyanto35@gmail.com>
2024-05-15 14:56:05 +0700

    update: js/dashboard/app

--- resources/js/dashboard/app.js
+++ resources/js/dashboard/app.js
@@ -1,16 +1,56 @@
+// Memeriksa apakah currentUser ada dan role-nya adalah "kepala sekolah" atau "guru"
 if (
-    window.location.pathname == "/dashboard" ||
-    window.location.pathname == "/dashboard/"
+    window.currentUser &&
+    (window.currentUser.role === "kepala sekolah" ||
+        window.currentUser.role === "guru")
 ) {
-    import("./rankChart")
-        .then((module) => {})
-        .catch((error) => {
-            console.error("Gagal mengimpor rankChart");
-        });
+    // Memeriksa apakah pengguna berada di dashboard
+    if (
+        window.location.pathname === "/dashboard" ||
+        window.location.pathname === "/dashboard/"
+    ) {
+        // Mengimpor selfRankChart
+        import("./selfRankChart")
+            .then((module) => {})
+            .catch((error) => {
+                console.error("Gagal mengimpor selfRankChart");
+            });
 
-    import("./rankTable")
-        .then((module) => {})
-        .catch((error) => {
-            console.error("Gagal mengimpor rankTable");
-        });
+        // Mengimpor rankChart
+        import("./rankChart")
+            .then((module) => {})
+            .catch((error) => {
+                console.error("Gagal mengimpor rankChart");
+            });
+
+        // Mengimpor rankTable
+        import("./rankTable")
+            .then((module) => {})
+            .catch((error) => {
+                console.error("Gagal mengimpor rankTable");
+            });
+    }
+}
+
+// Memeriksa apakah currentUser ada dan role-nya adalah "superadmin", "yayasan", "deputi", "IT", atau "admin"
+if (
+    window.currentUser &&
+    (window.currentUser.role === "superadmin" ||
+        window.currentUser.role === "yayasan" ||
+        window.currentUser.role === "deputi" ||
+        window.currentUser.role === "IT" ||
+        window.currentUser.role === "admin")
+) {
+    // Memeriksa apakah pengguna berada di dashboard
+    if (
+        window.location.pathname === "/dashboard" ||
+        window.location.pathname === "/dashboard/"
+    ) {
+        // Mengimpor rankGroupChart
+        import("./rankGroupChart")
+            .then((module) => {})
+            .catch((error) => {
+                console.error("Gagal mengimpor rankGroupChart");
+            });
+    }
 }
