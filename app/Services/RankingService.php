870b67e1c3e6b429aba1faa053bd8ec444854f86
danisec <daniaprilyanto35@gmail.com>
2024-05-15 14:49:21 +0700

    update: RankingService

--- app/Services/RankingService.php
+++ app/Services/RankingService.php
@@ -15,15 +15,15 @@ class RankingService
             foreach ($kriteria as $kriteriaItem) {
                 $bobotPrioritasSubkriteriaItem = $bobotPrioritasSubkriteria->where('kriteria.kode_kriteria', $kriteriaItem->kode_kriteria)->first();
 
-                $bobotPrioritasAlternatifItem = $bobotAlternatif->where('kode_kriteria', $kriteriaItem->kode_kriteria)->where('kode_alternatif', $alternatifItem->alternatifKedua->kode_alternatif)->first();
+                $bobotPrioritasAlternatifItem = $bobotAlternatif->where('kode_kriteria', $kriteriaItem->kode_kriteria)->where('kode_alternatif', $alternatifItem->alternatifKedua->alternatifPertama->kode_alternatif)->first();
 
                 if ($bobotPrioritasSubkriteriaItem && $bobotPrioritasAlternatifItem) {
                     $total += ($bobotPrioritasSubkriteriaItem->bobot_prioritas * $bobotPrioritasAlternatifItem->bobot_prioritas);
                 }
             }
 
-            $totalBobotKriteria[$alternatifItem->alternatifKedua->kode_alternatif] = $total;
-            $totalBobotKriteria[$alternatifItem->alternatifKedua->kode_alternatif] = substr($total, 0, 6);
+            $totalBobotKriteria[$alternatifItem->alternatifKedua->alternatifPertama->kode_alternatif] = $total;
+            $totalBobotKriteria[$alternatifItem->alternatifKedua->alternatifPertama->kode_alternatif] = substr($total, 0, 6);
         }
 
         return $totalBobotKriteria;
