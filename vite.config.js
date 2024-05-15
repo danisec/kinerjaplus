c8794ee4b7ddf091622937448b941aebb3cf55b5
danisec <daniaprilyanto35@gmail.com>
2024-05-15 11:03:57 +0700

    add: resources/css/multi-select-tag.css

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,11 @@ export default defineConfig({
     },
     plugins: [
         laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
+            input: [
+                "resources/css/app.css",
+                "resources/css/multi-select-tag.css",
+                "resources/js/app.js",
+            ],
             refresh: true,
         }),
     ],
