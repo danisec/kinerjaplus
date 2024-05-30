252f308de65984191947902378e29c375ddefb32
danisec <daniaprilyanto35@gmail.com>
2024-05-31 04:09:06 +0700

    update: NilaiSkalaSeeder

--- database/seeders/NilaiSkalaSeeder.php
+++ database/seeders/NilaiSkalaSeeder.php
@@ -12,6 +12,35 @@ class NilaiSkalaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $nilaiSkala = [
+            [
+                'skala' => '1',
+                'nilai_skala' => '70',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'skala' => '2',
+                'nilai_skala' => '75',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'skala' => '3',
+                'nilai_skala' => '85',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'skala' => '4',
+                'nilai_skala' => '100',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($nilaiSkala as $data) {
+            \App\Models\NilaiSkala::create($data);
+        }
     }
 }
