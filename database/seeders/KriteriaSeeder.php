a560ae097e28b88fe9b3901c020e6fb31c3a41be
danisec <daniaprilyanto35@gmail.com>
2024-05-31 04:08:49 +0700

    update: KriteriaSeeder

--- database/seeders/KriteriaSeeder.php
+++ database/seeders/KriteriaSeeder.php
@@ -16,25 +16,28 @@ class KriteriaSeeder extends Seeder
             [
                 'kode_kriteria' => 'K1',
                 'nama_kriteria' => 'Kompetensi',
+                'bobot_kriteria' => '40',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'kode_kriteria' => 'K2',
                 'nama_kriteria' => 'Keterampilan Mengelola Proses Belajar Mengajar',
+                'bobot_kriteria' => '40',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
                 'kode_kriteria' => 'K3',
                 'nama_kriteria' => 'Kepatuhan',
+                'bobot_kriteria' => '20',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
         ];
 
         foreach ($kriteria as $data) {
-            \App\Models\Subkriteria::create($data);
+            \App\Models\Kriteria::create($data);
         }
     }
 }
