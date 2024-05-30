2b4f6619efcea52f923846c8111f1697927b47cf
danisec <daniaprilyanto35@gmail.com>
2024-05-31 04:09:20 +0700

    update: PerhitunganKriteriaSeeder

--- database/seeders/PerhitunganKriteriaSeeder.php
+++ database/seeders/PerhitunganKriteriaSeeder.php
@@ -12,6 +12,74 @@ class PerhitunganKriteriaSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $perhitunganKriteria = [
+            [
+                'kriteria_pertama' => 'K1',
+                'kriteria_kedua' => 'K1',
+                'nilai_kriteria' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K1',
+                'kriteria_kedua' => 'K2',
+                'nilai_kriteria' => '3',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K1',
+                'kriteria_kedua' => 'K3',
+                'nilai_kriteria' => '5',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K2',
+                'kriteria_kedua' => 'K1',
+                'nilai_kriteria' => '0.3333',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K2',
+                'kriteria_kedua' => 'K2',
+                'nilai_kriteria' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K2',
+                'kriteria_kedua' => 'K3',
+                'nilai_kriteria' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K3',
+                'kriteria_kedua' => 'K1',
+                'nilai_kriteria' => '0.2',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K3',
+                'kriteria_kedua' => 'K2',
+                'nilai_kriteria' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+            [
+                'kriteria_pertama' => 'K3',
+                'kriteria_kedua' => 'K3',
+                'nilai_kriteria' => '1',
+                'created_at' => now(),
+                'updated_at' => now(),
+            ],
+        ];
+
+        foreach ($perhitunganKriteria as $data) {
+            \App\Models\PerhitunganKriteria::create($data);
+        }
     }
 }
