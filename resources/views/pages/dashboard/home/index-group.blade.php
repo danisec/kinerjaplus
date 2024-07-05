982cdbc0bb7e9c8ae7d0937e9cd312730370c805
danisec <daniaprilyanto35@gmail.com>
2024-07-06 02:00:22 +0700

    update: home/index-group

--- resources/views/pages/dashboard/home/index-group.blade.php
+++ resources/views/pages/dashboard/home/index-group.blade.php
@@ -1,5 +1,119 @@
 <x-layouts.app-dashboard title="{{ $title }}">
 
+    <div class="my-2">
+        @if ($checkGroupKaryawan != null)
+            @if (in_array(Auth::user()->role, ['yayasan', 'deputi']))
+                <div class="mb-12">
+                    <div class="flex flex-row items-center gap-4">
+                        <input id="kodeAlternatif" name="kode_alternatif" type="hidden"
+                            value="{{ $checkGroupKaryawan->alternatif->kode_alternatif }}">
+                        <h4 class="text-xl font-bold text-gray-700" id="namaGroup"
+                            data-nama-group="{{ $checkGroupKaryawan->alternatif->nama_alternatif }}">Kinerja Karyawan
+                            {!! $checkGroupKaryawan->alternatif->nama_alternatif !!} Per Tahun Ajaran
+                        </h4>
+                    </div>
+
+                    <div class="container mt-6">
+                        <div class="rounded-md bg-white p-4 shadow-md shadow-slate-100">
+                            <div id="selfRanking"></div>
+
+                            <div class="animate-pulse" id="loadingSelfChart" role="status">
+                                <div class="mb-2.5 h-2 w-48 rounded-full bg-slate-100"></div>
+                                <div class="mt-4 flex items-baseline">
+                                    <div class="ms-6 h-56 w-full rounded-t-lg bg-slate-100"></div>
+                                    <div class="ms-6 h-72 w-full rounded-t-lg bg-slate-100"></div>
+                                    <div class="ms-6 h-64 w-full rounded-t-lg bg-slate-100"></div>
+                                    <div class="ms-6 h-80 w-full rounded-t-lg bg-slate-100"></div>
+                                    <div class="ms-6 h-72 w-full rounded-t-lg bg-slate-100"></div>
+                                </div>
+                                <span class="sr-only">Loading...</span>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="relative overflow-x-auto rounded-lg shadow-sm">
+                    <table class="w-full text-left text-base text-gray-900">
+                        <thead class="bg-slate-100 text-sm uppercase text-gray-900">
+                            <tr>
+                                <th class="px-6 py-3" scope="col">
+                                    No.
+                                </th>
+                                <th class="px-6 py-3" scope="col">
+                                    <div class="flex flex-row items-center gap-0.5">
+                                        @sortablelink('tahun_ajaran', 'Tahun Ajaran')
+
+                                        <x-atoms.svg.arrow-down @class(['mt-0.5 h-4 w-4']) />
+                                    </div>
+                                </th>
+                                <th class="px-6 py-3" scope="col">
+                                    Nama Karyawan
+                                </th>
+                                <th class="px-6 py-3" scope="col">
+                                    <div class="flex flex-row items-center gap-0.5">
+                                        @sortablelink('nilai', 'Nilai Kinerja')
+
+                                        <x-atoms.svg.arrow-down @class(['mt-0.5 h-4 w-4']) />
+                                    </div>
+                                </th>
+                                <th class="px-6 py-3 text-center" scope="colgroup">
+                                    <div class="flex flex-row items-center justify-center gap-0.5">
+                                        @sortablelink('rank', 'Rank')
+
+                                        <x-atoms.svg.arrow-down @class(['mt-0.5 h-4 w-4']) />
+                                    </div>
+                                </th>
+                            </tr>
+                        </thead>
+
+                        @if ($selfRankings != null && $selfRankings->count() > 0)
+                            @foreach ($selfRankings as $item)
+                                <tbody>
+                                    <tr class="border-b bg-white hover:bg-slate-100">
+                                        <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900"
+                                            scope="row">
+                                            {{ ($selfRankings->currentPage() - 1) * $selfRankings->perPage() + $loop->iteration }}
+                                        </th>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ $item->tahun_ajaran }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ $item->alternatif->alternatifPertama->nama_alternatif }}
+                                        </td>
+                                        <td class="whitespace-nowrap px-6 py-4">
+                                            {{ substr($item->nilai, 0, 9) }}
+                                        </td>
+                                        <td class="flex justify-center gap-4 px-6 py-4">
+                                            {{ $item->rank }}
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            @endforeach
+                        @else
+                            <tbody>
+                                <tr class="border-b bg-white">
+                                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="6">
+                                        Data belum ada.
+                                    </td>
+                                </tr>
+                            </tbody>
+                        @endif
+                    </table>
+                </div>
+
+                <div class="bg-white p-6">
+                    {{ $selfRankings->links('vendor.pagination.tailwind') }}
+                </div>
+            @endif
+        @else
+            <div class="flex flex-col items-center justify-center">
+                <h2>Anda belum terdaftar di group karyawan. Hubungi admin untuk mendaftarkan diri Anda ke dalam
+                    group karyawan.</h2>
+            </div>
+        @endif
+    </div>
+
     @if (in_array(Auth::user()->role, ['yayasan', 'deputi']))
         <div class="my-2">
             <div class="flex flex-row items-center gap-4">
@@ -20,8 +134,9 @@
 
                 <div class="w-64">
                     <select class="field-input-slate w-full" id="selectNamaGroup" name="nama_group_karyawan"
-                        data-nama-group="{{ $firstNamaGroupKaryawan->nama_group_karyawan }}">
-                        <option selected disabled hidden>{{ $firstNamaGroupKaryawan->nama_group_karyawan }}</option>
+                        data-nama-group="{{ $firstNamaGroupKaryawan ? $firstNamaGroupKaryawan->nama_group_karyawan : '' }}">
+                        <option selected disabled hidden>
+                            {{ $firstNamaGroupKaryawan ? $firstNamaGroupKaryawan->nama_group_karyawan : '' }}</option>
                         @foreach ($namaGroupKaryawan as $nama)
                             <option value="{{ $nama }}">
                                 {{ $nama }}
