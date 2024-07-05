bd36390d4823915f9b1bf7c86fc5385ec6d50466
danisec <daniaprilyanto35@gmail.com>
2024-07-06 02:01:05 +0700

    update: guru/penilaian/create

--- resources/views/pages/guru/penilaian/create.blade.php
+++ resources/views/pages/guru/penilaian/create.blade.php
@@ -47,7 +47,7 @@
 
                         <select
                             class="@error('alternatif_kedua') border-red-500 @enderror field-input-slate w-full font-semibold"
-                            name="alternatif_kedua" autofocus required>
+                            id="alternatif_kedua" name="alternatif_kedua" autofocus required>
 
                             <option selected disabled hidden>Pilih Nama Karyawan</option>
 
@@ -69,54 +69,62 @@
                     </div>
                 </div>
 
-                <div>
+                <div id="kriteriaContainer">
+                    <?php
+                    $kriteriaCounter = 1;
+                    ?>
+
                     @foreach ($kriteria as $item)
-                        <div class="mb-6 rounded-md bg-slate-50 p-4">
-                            <h4 class="block text-xl font-semibold text-gray-900">
-                                {{ chr(64 + $loop->iteration) . '.' }} {{ $item->nama_kriteria }}
-                            </h4>
+                        @if (
+                            !in_array(Auth::user()->role, ['tata usaha non tenaga pendidikan', 'kerohanian non tenaga pendidikan']) ||
+                                $item->kode_kriteria != 'K2')
+                            <div class="mb-6 rounded-md bg-slate-50 p-4">
+                                <h4 class="block text-xl font-semibold text-gray-900">
+                                    {{ chr(64 + $kriteriaCounter++) . '.' }} {{ $item->nama_kriteria }}
+                                </h4>
 
-                            @foreach ($item->subkriteria as $subkriteria)
-                                <h6 class="ml-5 pb-2 pt-5 text-lg font-semibold text-gray-900">
-                                    {{ $loop->iteration . '.' }} {{ $subkriteria->nama_subkriteria }}
-                                </h6>
+                                @foreach ($item->subkriteria as $subkriteria)
+                                    <h6 class="ml-5 pb-2 pt-5 text-lg font-semibold text-gray-900">
+                                        {{ $loop->iteration . '.' }} {{ $subkriteria->nama_subkriteria }}
+                                    </h6>
 
-                                @foreach ($subkriteria->indikatorSubkriteria as $indikator)
-                                    <div class="mb-3">
-                                        <p class="ml-10 text-base font-medium text-gray-900">
-                                            {{ $loop->iteration . ')' }} {{ $indikator->indikator_subkriteria }}
-                                        </p>
-                                    </div>
-
-                                    @foreach ($indikator->skalaIndikator as $skalaIndikator)
-                                        <div class="my-8 ml-10 flex flex-row gap-4">
-                                            @foreach ($skalaIndikator->skalaIndikatorDetail as $skalaIndikatorDetail)
-                                                <div
-                                                    class="flex h-max w-auto flex-col items-center justify-center gap-3 rounded-md bg-white p-3 shadow-slate-50 hover:shadow-md">
-                                                    <input
-                                                        name="id_skala_indikator_detail[{{ $skalaIndikator->id_indikator_subkriteria }}]"
-                                                        type="radio"
-                                                        value="{{ $skalaIndikatorDetail->id_skala_indikator_detail }}"
-                                                        {{ old('id_indikator_subkriteria.' . $skalaIndikator->id_indikator_subkriteria) == $skalaIndikatorDetail->skala
-                                                            ? 'checked'
-                                                            : '' }}
-                                                        required>
-
-                                                    <label
-                                                        class="text-left text-base font-normal leading-normal text-gray-900"
-                                                        for="{{ $skalaIndikatorDetail->skala }}">
-                                                        {{ $skalaIndikatorDetail->skala == 1 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
-                                                        {{ $skalaIndikatorDetail->skala == 2 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
-                                                        {{ $skalaIndikatorDetail->skala == 3 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
-                                                        {{ $skalaIndikatorDetail->skala == 4 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
-                                                    </label>
-                                                </div>
-                                            @endforeach
+                                    @foreach ($subkriteria->indikatorSubkriteria as $indikator)
+                                        <div class="mb-3">
+                                            <p class="ml-10 text-base font-medium text-gray-900">
+                                                {{ $loop->iteration . ')' }} {{ $indikator->indikator_subkriteria }}
+                                            </p>
                                         </div>
+
+                                        @foreach ($indikator->skalaIndikator as $skalaIndikator)
+                                            <div class="my-8 ml-10 flex flex-row gap-4">
+                                                @foreach ($skalaIndikator->skalaIndikatorDetail as $skalaIndikatorDetail)
+                                                    <div
+                                                        class="flex h-max w-auto flex-col items-center justify-center gap-3 rounded-md bg-white p-3 shadow-slate-50 hover:shadow-md">
+                                                        <input
+                                                            name="id_skala_indikator_detail[{{ $skalaIndikator->id_indikator_subkriteria }}]"
+                                                            type="radio"
+                                                            value="{{ $skalaIndikatorDetail->id_skala_indikator_detail }}"
+                                                            {{ old('id_indikator_subkriteria.' . $skalaIndikator->id_indikator_subkriteria) == $skalaIndikatorDetail->skala
+                                                                ? 'checked'
+                                                                : '' }}
+                                                            required>
+
+                                                        <label
+                                                            class="text-left text-base font-normal leading-normal text-gray-900"
+                                                            for="{{ $skalaIndikatorDetail->skala }}">
+                                                            {{ $skalaIndikatorDetail->skala == 1 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
+                                                            {{ $skalaIndikatorDetail->skala == 2 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
+                                                            {{ $skalaIndikatorDetail->skala == 3 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
+                                                            {{ $skalaIndikatorDetail->skala == 4 ? $skalaIndikatorDetail->deskripsi_skala : '' }}
+                                                        </label>
+                                                    </div>
+                                                @endforeach
+                                            </div>
+                                        @endforeach
                                     @endforeach
                                 @endforeach
-                            @endforeach
-                        </div>
+                            </div>
+                        @endif
                     @endforeach
                 </div>
 
