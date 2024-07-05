da4ba988879a4a48da4dbde3802033c3ce11d4ef
danisec <daniaprilyanto35@gmail.com>
2024-07-06 01:52:26 +0700

    update: components/aside

--- resources/views/components/organism/aside.blade.php
+++ resources/views/components/organism/aside.blade.php
@@ -46,7 +46,16 @@
                     </li>
                 @endif
 
-                @if (in_array(Auth::user()->role, ['kepala sekolah', 'guru']))
+                @if (in_array(Auth::user()->role, [
+                        'yayasan',
+                        'deputi',
+                        'kepala sekolah',
+                        'guru',
+                        'tata usaha tenaga pendidikan',
+                        'tata usaha non tenaga pendidikan',
+                        'kerohanian tenaga pendidikan',
+                        'kerohanian non tenaga pendidikan',
+                    ]))
                     <li class="flex flex-col gap-2.5">
                         @foreach ($sideNavPenilaian as $name => $data)
                             <a class="{{ request()->segment(2) == $data['url'] || request()->segment(2) == 'penilaian'
@@ -66,7 +75,16 @@
                     </li>
                 @endif
 
-                @if (in_array(Auth::user()->role, ['kepala sekolah', 'guru']))
+                @if (in_array(Auth::user()->role, [
+                        'yayasan',
+                        'deputi',
+                        'kepala sekolah',
+                        'guru',
+                        'tata usaha tenaga pendidikan',
+                        'tata usaha non tenaga pendidikan',
+                        'kerohanian tenaga pendidikan',
+                        'kerohanian non tenaga pendidikan',
+                    ]))
                     <li class="flex flex-col gap-2.5">
                         @foreach ($sideNavGuru as $name => $data)
                             <a class="{{ request()->segment(2) == $data['url']
@@ -114,10 +132,21 @@
                     <li class="flex flex-col gap-2.5" x-data="{ isOpen: localStorage.getItem('navSideDropDown') === 'true' || false }">
                         <a class="flex flex-row items-center gap-2 rounded-lg p-2 text-base text-gray-900 hover:bg-slate-100"
                             href="#" @click="isOpen = !isOpen; localStorage.setItem('navSideDropDown', isOpen)">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
-                                viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor">
-                                <path stroke-linecap="round" stroke-linejoin="round"
-                                    d="M20.25 6.375c0 2.278-3.694 4.125-8.25 4.125S3.75 8.653 3.75 6.375m16.5 0c0-2.278-3.694-4.125-8.25-4.125S3.75 4.097 3.75 6.375m16.5 0v11.25c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125V6.375m16.5 0v3.75m-16.5-3.75v3.75m16.5 0v3.75C20.25 16.153 16.556 18 12 18s-8.25-1.847-8.25-4.125v-3.75m16.5 0c0 2.278-3.694 4.125-8.25 4.125s-8.25-1.847-8.25-4.125" />
+
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="20" height="20"
+                                color="#000000" fill="none">
+                                <path
+                                    d="M14.5405 2V4.48622C14.5405 6.23417 14.5405 7.10814 14.7545 7.94715C14.9685 8.78616 15.3879 9.55654 16.2267 11.0973L17.3633 13.1852C19.5008 17.1115 20.5696 19.0747 19.6928 20.53L19.6792 20.5522C18.7896 22 16.5264 22 12 22C7.47357 22 5.21036 22 4.3208 20.5522L4.30725 20.53C3.43045 19.0747 4.49918 17.1115 6.63666 13.1852L7.7733 11.0973C8.61209 9.55654 9.03149 8.78616 9.24548 7.94715C9.45947 7.10814 9.45947 6.23417 9.45947 4.48622V2"
+                                    stroke="currentColor" stroke-width="1.5" />
+                                <path d="M9 16.002L9.00868 15.9996" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M15 18.002L15.0087 17.9996" stroke="currentColor" stroke-width="2"
+                                    stroke-linecap="round" stroke-linejoin="round" />
+                                <path d="M8 2L16 2" stroke="currentColor" stroke-width="1.5" stroke-linecap="round"
+                                    stroke-linejoin="round" />
+                                <path
+                                    d="M7.5 11.5563C8.5 10.4029 10.0994 11.2343 12 12.3182C14.5 13.7439 16 12.65 16.5 11.6152"
+                                    stroke="currentColor" stroke-width="1.5" stroke-linecap="round" />
                             </svg>
 
                             Perbandingan
@@ -153,7 +182,10 @@
                                 <svg class="{{ request()->segment(2) == $data['url'] ? 'text-gray-900 transition duration-75' : 'text-gray-700 group-hover:text-gray-700' }}"
                                     xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none"
                                     viewBox="{{ $data['viewBox'] }}" stroke-width="1.5" stroke="currentColor">
-                                    <path stroke-linecap="round" stroke-linejoin="round" d="{{ $data['path'] }}" />
+                                    @foreach ($data['paths'] as $path)
+                                        <path stroke-linecap="round" stroke-linejoin="round"
+                                            d="{{ $path }}" />
+                                    @endforeach
                                 </svg>
 
                                 <span>{{ $name }}</span>
