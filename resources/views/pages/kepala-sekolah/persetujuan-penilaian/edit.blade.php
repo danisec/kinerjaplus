5bc247eef461b5d98b249e71e82d146f27238dd3
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:41:39 +0700

    update: kepala-sekolah/persetujuan-penilaian/edit

--- resources/views/pages/kepala-sekolah/persetujuan-penilaian/edit.blade.php
+++ resources/views/pages/kepala-sekolah/persetujuan-penilaian/edit.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -53,4 +53,4 @@
         </div>
     </form>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
