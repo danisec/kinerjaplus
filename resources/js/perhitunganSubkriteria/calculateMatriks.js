6e6de3c6007aff89acf3c0ea0b729d1717ac922a
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:18:23 +0700

    update: js/perhitunganSubkriteria/calculateMatriks

--- resources/js/perhitunganSubkriteria/calculateMatriks.js
+++ resources/js/perhitunganSubkriteria/calculateMatriks.js
@@ -18,5 +18,10 @@ $(document).ready(function () {
         if (calculatedInput.val() === "0") {
             calculatedInput.val(calculatedValue);
         }
+
+        // Buatkan kondisi NaN jika inputan kosong
+        if (isNaN(calculatedValue)) {
+            calculatedInput.val("");
+        }
     });
 });
