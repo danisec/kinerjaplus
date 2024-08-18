494276304683be4144375cafe39d853387217252
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:18:40 +0700

    update: js/textArea/app

--- resources/js/textArea/app.js
+++ resources/js/textArea/app.js
@@ -3,7 +3,8 @@ if (
     window.location.pathname.includes("/dashboard/data-skala-indikator/") ||
     window.location.pathname.includes("/dashboard/data-penilaian/") ||
     window.location.pathname.includes("/dashboard/catatan-karyawan/") ||
-    window.location.pathname.includes("/dashboard/data-group-alternatif/")
+    window.location.pathname.includes("/dashboard/data-group-alternatif/") ||
+    window.location.pathname.includes("/dashboard/kelola-akun/view-akun/")
 ) {
     import("./textAreaHeight")
         .then((module) => {})
