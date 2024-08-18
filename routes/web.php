682a8174f1d0143dee9ff14915686d9f3029ab17
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:49:55 +0700

    update: routes/web

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\PenilaianController;
 use App\Http\Controllers\PerhitunganKriteriaController;
 use App\Http\Controllers\PerhitunganSubkriteriaController;
 use App\Http\Controllers\RiwayatPenilaianController;
+use App\Http\Controllers\TanggalPenilaianController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -19,49 +20,61 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::controller(DashboardController::class)->name('dashboard.')->middleware('auth')->group(function () {
-    Route::get('/dashboard', 'index')->name('index')->middleware('user-role:superadmin,yayasan,kepala sekolah,deputi,guru,IT,admin,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan');
+Route::controller(DashboardController::class)->name('dashboard.')->middleware('auth', 'permission:dashboard')->group(function () {
+    Route::get('/dashboard', 'index')->name('index');
 
-    Route::get('/dashboard/{kodeAlternatif}/getSelfRankChart', 'getSelfRankChart')->name('getSelfRankChart')->middleware('user-role:yayasan,deputi,kepala sekolah,guru,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan');
+    Route::get('/dashboard/{kodeAlternatif}/getSelfRankChart', 'getSelfRankChart')->name('getSelfRankChart')->middleware('role:yayasan|deputi|kepala sekolah|guru|tata usaha tenaga pendidikan|tata usaha non tenaga pendidikan|kerohanian tenaga pendidikan|kerohanian non tenaga pendidikan');
 
-    Route::get('/dashboard/{firstYear}/{secondYear}/getRankTahunAjaranChart', 'getRankTahunAjaranChart')->name('getRankTahunAjaranChart')->middleware('user-role:kepala sekolah,guru,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan');
-    Route::get('/dashboard/{firstYear}/{secondYear}/getRankTahunAjaranTable', 'getRankTahunAjaranTable')->name('getRankTahunAjaranTable')->middleware('user-role:kepala sekolah,guru,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan');
+    Route::get('/dashboard/{idTanggalPenilaian}/getRankTahunAjaranChart', 'getRankTahunAjaranChart')->name('getRankTahunAjaranChart')->middleware('role:kepala sekolah|guru|tata usaha tenaga pendidikan|tata usaha non tenaga pendidikan|kerohanian tenaga pendidikan|kerohanian non tenaga pendidikan');
 
-    Route::get('/dashboard/{firstYear}/{secondYear}/{namaGroupKaryawan}/getRankTahunAjaranGroupChart', 'getRankTahunAjaranGroupChart')->name('getRankTahunAjaranGroupChart')->middleware('user-role:superadmin,yayasan,deputi,IT,admin');
-    Route::get('/dashboard/{firstYear}/{secondYear}/{namaGroupKaryawan}/getRankTahunAjaranGroupTable', 'getRankTahunAjaranGroupTable')->name('getRankTahunAjaranGroupTable')->middleware('user-role:superadmin,yayasan,deputi,IT,admin');
+    Route::get('/dashboard/{idTanggalPenilaian}/getRankTahunAjaranTable', 'getRankTahunAjaranTable')->name('getRankTahunAjaranTable')->middleware('role:kepala sekolah|guru|tata usaha tenaga pendidikan|tata usaha non tenaga pendidikan|kerohanian tenaga pendidikan|kerohanian non tenaga pendidikan');
+
+    Route::get('/dashboard/{idTanggalPenilaian}/{namaGroupKaryawan}/getRankTahunAjaranGroupChart', 'getRankTahunAjaranGroupChart')->name('getRankTahunAjaranGroupChart')->middleware('role:yayasan|deputi');
+    
+    Route::get('/dashboard/{idTanggalPenilaian}/{namaGroupKaryawan}/getRankTahunAjaranGroupTable', 'getRankTahunAjaranGroupTable')->name('getRankTahunAjaranGroupTable')->middleware('role:yayasan|deputi');
 });
 
-Route::controller(PenilaianController::class)->name('penilaian.')->middleware('auth', 'user-role:yayasan,deputi,kepala sekolah,guru,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan')->group(function () {
+Route::controller(TanggalPenilaianController::class)->name('tanggalPenilaian.')->middleware('auth', 'permission:penilaian')->group(function () {
+    Route::get('/dashboard/penilaian/buat-tanggal-penilaian', 'create')->name('create');
+    Route::post('/dashboard/penilaian/buat-tanggal-penilaian', 'store')->name('store');
+});
+
+Route::controller(PenilaianController::class)->name('penilaian.')->middleware('auth', 'permission:penilaian')->group(function () {
     Route::get('/dashboard/penilaian/introduction', 'welcome')->name('welcome');
+    
     Route::get('/dashboard/penilaian/tambah-penilaian', 'create')->name('create');
+    Route::post('/dashboard/penilaian/introduction', 'store')->name('store');
 
     Route::get('/dashboard/penilaian/tambah-penilaian/get-kriteria/{kodeAlternatif}', 'getKriteria')->name('getKriteria');
     
-    Route::post('/dashboard/penilaian/introduction', 'store')->name('store');
 });
 
-Route::controller(RiwayatPenilaianController::class)->name('riwayatPenilaian.')->middleware('auth', 'user-role:yayasan,deputi,kepala sekolah,guru,tata usaha tenaga pendidikan,tata usaha non tenaga pendidikan,kerohanian tenaga pendidikan,kerohanian non tenaga pendidikan')->group(function () {
+Route::controller(RiwayatPenilaianController::class)->name('riwayatPenilaian.')->middleware('auth', 'permission:riwayat-penilaian')->group(function () {
     Route::get('/dashboard/riwayat-penilaian', 'index')->name('index');
-    Route::get('/dashboard/riwayat-penilaian/view-penilaian/{id}', 'show')->name('show');
-    
-    Route::get('/dashboard/riwayat-penilaian/{firstYear}/{secondYear}', 'showTahun')->name('showTahun');
+
+    Route::get('/dashboard/riwayat-penilaian/{firstYear}/{secondYear}/{semester}', 'showTahun')->name('showTahun');
+    Route::get('/dashboard/riwayat-penilaian/{id}/view-penilaian', 'show')->name('show');
 });
 
 Route::controller(PerhitunganKriteriaController::class)->name('perhitunganKriteria.')->middleware('auth')->group(function () {
-    Route::get('/dashboard/perbandingan-kriteria', 'index')->name('index')->middleware('user-role:superadmin');
-    Route::get('/dashboard/perbandingan-kriteria/hasil-perbandingan-kriteria', 'hasil')->name('hasil')->middleware('user-role:superadmin,kepala sekolah');
+    Route::get('/dashboard/perbandingan-kriteria', 'index')->name('index')->middleware('role:superadmin');
+    Route::get('/dashboard/perbandingan-kriteria/pedoman', 'pedoman')->name('pedoman')->middleware('role:superadmin');
 
-    Route::post('/dashboard/perbandingan-kriteria', 'store')->name('store')->middleware('user-role:superadmin');
+    Route::get('/dashboard/perbandingan-kriteria/hasil-perbandingan-kriteria', 'hasil')->name('hasil')->middleware('role:superadmin|kepala sekolah');
+    
+    Route::post('/dashboard/perbandingan-kriteria', 'store')->name('store')->middleware('role:superadmin');
 });
 
 Route::controller(PerhitunganSubkriteriaController::class)->name('perhitunganSubkriteria.')->middleware('auth')->group(function () {
-    Route::get('/dashboard/perbandingan-subkriteria', 'index')->name('index')->middleware('user-role:superadmin');
-    Route::get('/dashboard/perbandingan-subkriteria/hasil-perbandingan-subkriteria', 'hasil')->name('hasil')->middleware('user-role:superadmin,kepala sekolah');
+    Route::get('/dashboard/perbandingan-subkriteria', 'index')->name('index')->middleware('role:superadmin');
+    Route::get('/dashboard/perbandingan-subkkriteria/pedoman', 'pedoman')->name('pedoman')->middleware('role:superadmin');
 
-    Route::post('/dashboard/perbandingan-subkriteria', 'store')->name('store')->middleware('user-role:superadmin');
+    Route::get('/dashboard/perbandingan-subkriteria/hasil-perbandingan-subkriteria', 'hasil')->name('hasil')->middleware('role:superadmin|kepala sekolah');
+
+    Route::post('/dashboard/perbandingan-subkriteria', 'store')->name('store')->middleware('role:superadmin');
 });
 
-Route::controller(KelolaAkunController::class)->name('kelolaAkun.')->middleware('auth', 'user-role:superadmin,IT')->group(function () {
+Route::controller(KelolaAkunController::class)->name('kelolaAkun.')->middleware('auth', 'permission:kelola-akun')->group(function () {
     Route::get('/dashboard/kelola-akun', 'index')->name('index');
 
     Route::get('/dashboard/kelola-akun/tambah-akun', 'create')->name('create');
