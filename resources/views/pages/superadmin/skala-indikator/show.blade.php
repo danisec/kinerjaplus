b5da627fd2dd5b34f7b517b542439eb11e59e8b1
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:48:30 +0700

    update: superadmin/skala-indikator/show

--- resources/views/pages/superadmin/skala-indikator/show.blade.php
+++ resources/views/pages/superadmin/skala-indikator/show.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -60,4 +60,4 @@
 
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
