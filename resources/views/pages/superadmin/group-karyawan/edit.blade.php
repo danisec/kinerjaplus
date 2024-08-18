5e422e3351d39122c14a70d1d6b63c6fa0a7bd7c
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:45:07 +0700

    update: superadmin/group-karyawan/edit

--- resources/views/pages/superadmin/group-karyawan/edit.blade.php
+++ resources/views/pages/superadmin/group-karyawan/edit.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -109,4 +109,4 @@
         </form>
     </div>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
