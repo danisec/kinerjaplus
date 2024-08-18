90304aabf472aa41666bcd5e4796c881efe11199
danisec <daniaprilyanto35@gmail.com>
2024-08-19 03:48:48 +0700

    update: superadmin/subkriteria/create

--- resources/views/pages/superadmin/subkriteria/create.blade.php
+++ resources/views/pages/superadmin/subkriteria/create.blade.php
@@ -1,4 +1,4 @@
-<x-layouts.app-dashboard title="{{ $title }}">
+<x-app-dashboard title="{{ $title }}">
 
     <x-molecules.breadcrumb>
         <li aria-current="page">
@@ -171,4 +171,4 @@
         });
     </script>
 
-</x-layouts.app-dashboard>
+</x-app-dashboard>
