d3a5e5d3bf08ae567b9f160c1f97e317c44b29dd
danisec <daniaprilyanto35@gmail.com>
2024-08-19 02:58:55 +0700

    update: config/app

--- config/app.php
+++ config/app.php
@@ -170,6 +170,7 @@ return [
         App\Providers\RouteServiceProvider::class,
         Kyslik\ColumnSortable\ColumnSortableServiceProvider::class,
         Jenssegers\Agent\AgentServiceProvider::class,
+        Spatie\Permission\PermissionServiceProvider::class,
     ])->toArray(),
 
     /*
