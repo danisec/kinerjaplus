c74bc9dc1d67d35553cf3147aec74d446ea8902a
danisec <daniaprilyanto35@gmail.com>
2024-09-23 05:35:21 +0700

    add: hasDirectPermission

--- app/Models/User.php
+++ app/Models/User.php
@@ -55,4 +55,9 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
         'password' => 'hashed',
     ];
+
+    public function hasDirectPermission($permission)
+    {
+        return $this->getDirectPermissions()->contains('name', $permission);
+    }
 }
