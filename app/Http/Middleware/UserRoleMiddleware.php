f7127761ca7a73a6fba86f7f41ce480f609d41e2
danisec <daniaprilyanto35@gmail.com>
2024-09-23 05:33:53 +0700

    add: handle function UserRoleMiddleware

--- app/Http/Middleware/UserRoleMiddleware.php
+++ app/Http/Middleware/UserRoleMiddleware.php
@@ -14,12 +14,31 @@ class UserRoleMiddleware
      *
      * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
      */
-    // public function handle(Request $request, Closure $next, ...$role): Response
-    // {
-    //     if (Auth::check() && in_array(Auth::user()->role, $role)) {
-    //         return $next($request);
-    //     }
-        
-    //     return redirect()->route('dashboard.index');
-    // }
+    public function handle($request, Closure $next, ...$permissions)
+    {
+        $user = Auth::user();
+
+        // Cek apakah pengguna memiliki direct permissions di tabel model_has_permissions
+        $directPermissionsExist = $user->getDirectPermissions()->isNotEmpty();
+
+        // Jika pengguna memiliki direct permissions, periksa apakah dia memiliki salah satu dari permissions yang diberikan
+        if ($directPermissionsExist) {
+            $hasDirectPermission = $user->hasAnyDirectPermission($permissions);
+            
+            if (!$hasDirectPermission) {
+                // Jika tidak memiliki direct permission yang diperlukan, return 403
+                abort(403, 'Unauthorized action.');
+            }
+        } else {
+            // Jika tidak ada direct permissions, periksa melalui role permissions
+            $hasRolePermission = $user->hasAnyPermission($permissions);
+            
+            if (!$hasRolePermission) {
+                // Jika tidak memiliki permissions baik secara langsung maupun melalui role, return 403
+                abort(403, 'Unauthorized action.');
+            }
+        }
+
+        return $next($request);
+    }
 }
