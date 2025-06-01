295279c62fa350235d2ce2fbee5612409d2ef297
danisec <daniaprilyanto35@gmail.com>
2025-06-02 06:45:34 +0700

    componen(ModalUpdate): add new modal update component

--- app/View/Components/molecules/modal/ModalUpdate.php
+++ app/View/Components/molecules/modal/ModalUpdate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\View\Components\molecules;
+namespace App\View\Components\molecules\modal;
 
 use Closure;
 use Illuminate\Contracts\View\View;
@@ -21,6 +21,6 @@ class ModalUpdate extends Component
      */
     public function render(): View|Closure|string
     {
-        return view('components.molecules.modal-update');
+        return view('components.molecules.modal.modal-update');
     }
 }
