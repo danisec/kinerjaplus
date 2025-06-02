a2341cfe3e570ea02d02d3cc9fabadfe79ae332c
danisec <daniaprilyanto35@gmail.com>
2025-06-02 19:29:29 +0700

    feat(skeleton-table.blade.php): add skeleton table component

---
+++ resources/views/components/atoms/skeleton/skeleton-table.blade.php
@@ -0,0 +1,6 @@
+<div class="mb-4 h-2.5 w-full rounded-full bg-slate-100"></div>
+<div class="mb-4 h-2.5 w-full rounded-full bg-slate-100"></div>
+<div class="mb-4 h-2.5 w-full rounded-full bg-slate-100"></div>
+<div class="mb-4 h-2.5 w-full rounded-full bg-slate-100"></div>
+<div class="mb-4 h-2.5 w-full rounded-full bg-slate-100"></div>
+<span class="sr-only">Loading...</span>
