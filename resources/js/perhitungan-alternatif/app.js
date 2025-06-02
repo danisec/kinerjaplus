b8de15b8763a2e4ea79d203a8dd46c6980386695
danisec <daniaprilyanto35@gmail.com>
2025-06-02 19:09:17 +0700

    feat(app.js): add dynamic import for calculate-matriks in perbandingan-alternatif

--- resources/js/perhitungan-alternatif/app.js
+++ resources/js/perhitungan-alternatif/app.js
@@ -1,7 +1,7 @@
 if (window.location.pathname.includes("/dashboard/perbandingan-alternatif/")) {
-    import("./calculateMatriks")
+    import("./calculate-matriks")
         .then((module) => {})
         .catch((error) => {
-            console.error("Gagal mengimpor calculateMatriks");
+            console.error("Gagal mengimpor calculate matriks");
         });
 }
