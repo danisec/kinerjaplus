26e5f8b26caacd9d71d5f7b639ef498a9ac7473c
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:06:57 +0700

    refactor(kelola-akun): improve breadcrumb structure and enhance table layout for better readability and user experience

--- resources/views/pages/it/kelola-akun/index.blade.php
+++ resources/views/pages/it/kelola-akun/index.blade.php
@@ -1,146 +1,125 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Kelola Akun</span>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Kelola Akun</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
     <div class="my-8">
-        <h4 class="mb-6 text-2xl font-semibold text-gray-900">Kelola Akun</h4>
-
-        <div class="flex flex-row items-center justify-between">
-            <div>
-                <x-molecules.search :placeholder="'Cari Akun'" :request="request('nama_kriteria')" :name="'fullname'" :value="request('fullname')" />
-            </div>
-
+        <div class="flex flex-row items-center justify-end">
             @can('direct.permission', 'kelola akun')
                 <div>
                     <a href="{{ route('kelolaAkun.create') }}">
-                        <x-atoms.button.button-primary :customClass="'h-12 w-36 rounded-md'" :type="'button'" :name="'Tambah Akun'" />
+                        <x-atoms.button.button-primary :type="'button'" :name="'Tambah Akun'" />
                     </a>
                 </div>
             @endcan
         </div>
     </div>
 
-    <div class="relative overflow-x-auto rounded-lg shadow-sm">
-        <table class="w-full text-left text-base text-gray-900">
-            <thead class="bg-slate-100 text-sm uppercase text-gray-900">
-                <tr>
-                    <th class="px-6 py-3" scope="col">
-                        No.
+    <div
+        class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 sm:px-6 dark:border-gray-800 dark:bg-white/[0.03]">
+        <x-molecules.table.table :title="'Daftar Pengguna'" :placeholderSearch="'Cari Pengguna'" :request="request('fullname')" :nameSearch="'fullname'">
+            <x-slot:thead>
+                <tr class="border-y border-gray-100 dark:border-gray-800">
+                    <th class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Nama Lengkap</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Nama Lengkap
+                    <th class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Peran</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Nama Pengguna
-                    </th>
-                    <th class="w-40 px-6 py-3 text-center" scope="col">
-                        Peran
-                    </th>
-                    <th class="flex justify-center px-6 py-3" scope="col">
-                        Aksi
+                    <th class="flex justify-center px-6 py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md text-center font-medium text-gray-800">Aksi
+                            </p>
+                        </div>
                     </th>
                 </tr>
-            </thead>
+            </x-slot:thead>
 
-            @if ($user != null && $user->count() > 0)
-                @foreach ($user as $item)
-                    <tbody>
-                        <tr class="border-b bg-white hover:bg-slate-100">
-                            <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                                {{ ($user->currentPage() - 1) * $user->perPage() + $loop->iteration }}
-                            </th>
-                            <td class="px-6 py-4">
+            @forelse ($user as $item)
+                <tr>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-sm sm:text-theme-md text-gray-700 dark:text-gray-400">
                                 {{ $item->fullname }}
-                            </td>
-                            <td class="px-6 py-4">
-                                {{ $item->username }}
-                            </td>
-                            <td class="px-6 py-4 text-center">
-                                <p
-                                    class="{{ $item->role == 'superadmin' || $item->role == 'admin'
-                                        ? 'bg-red-100/40 text-red-600'
-                                        : ($item->role == 'yayasan'
-                                            ? 'bg-emerald-100/40 text-emerald-600'
-                                            : ($item->role == 'deputi'
-                                                ? 'bg-violet-100/40 text-violet-600'
-                                                : ($item->role == 'kepala sekolah'
-                                                    ? 'bg-lime-100/40 text-lime-600'
-                                                    : ($item->role == 'guru'
-                                                        ? 'bg-fuchsia-100/40 text-fuchsia-600'
-                                                        : ($item->role == 'IT'
-                                                            ? 'bg-blue-100/40 text-blue-600'
-                                                            : 'bg-yellow-100/40 text-yellow-600'))))) }} whitespace-nowrap rounded-full px-2.5 py-1 capitalize">
+                            </p>
+                        </div>
+                    </td>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <p
+                                class="{{ $roleColors[$item->role] ?? $roleColors['default'] }} xs:text-xs sm:text-theme-sm rounded-full px-2 py-0.5 capitalize text-gray-700 dark:text-gray-400">
+                                {{ $item->role }}
+                            </p>
+                        </div>
+                    </td>
+                    <td class="xs:gap-2.5 flex justify-center p-6 sm:gap-4">
+                        @if (Auth::user()->hasAnyDirectPermission(['view kelola akun', 'kelola akun']) ||
+                                Auth::user()->canany(['view kelola akun', 'kelola akun']))
+                            <div x-data="{ showTooltip: false }">
+                                <a class="hover:text-blue-light-500 font-medium text-gray-600"
+                                    href="{{ route('kelolaAkun.show', $item->id) }}" @mouseenter="showTooltip = true"
+                                    @mouseleave="showTooltip = false">
+                                    <x-atoms.svg.eye />
+                                </a>
 
-                                    {{ $item->role }}
-                                </p>
-                            </td>
-                            <td class="flex justify-center gap-4 px-6 py-4">
-                                @if (Auth::user()->hasAnyDirectPermission(['view kelola akun', 'kelola akun']) ||
-                                        Auth::user()->canany(['view kelola akun', 'kelola akun']))
-                                    <div x-data="{ showTooltip: false }">
-                                        <a class="font-medium text-gray-600"
-                                            href="{{ route('kelolaAkun.show', $item->id) }}"
-                                            @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                            <x-atoms.svg.eye />
-                                        </a>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Show</span>
+                                </div>
+                            </div>
+                        @endif
 
-                                        <div class="absolute rounded bg-gray-100 px-2 py-1 text-sm text-gray-900"
-                                            x-show="showTooltip">
-                                            Detail
-                                        </div>
-                                    </div>
-                                @endif
+                        @can('direct.permission', 'kelola akun')
+                            <div x-data="{ showTooltip: false }">
+                                <a class="font-medium text-gray-600 hover:text-blue-600"
+                                    href="{{ route('kelolaAkun.edit', $item->id) }}" @mouseenter="showTooltip = true"
+                                    @mouseleave="showTooltip = false">
+                                    <x-atoms.svg.pen />
+                                </a>
 
-                                @can('direct.permission', 'kelola akun')
-                                    <div x-data="{ showTooltip: false }">
-                                        <a class="font-medium text-blue-600"
-                                            href="{{ route('kelolaAkun.edit', $item->id) }}"
-                                            @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                            <x-atoms.svg.pen />
-                                        </a>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Edit</span>
+                                </div>
+                            </div>
 
-                                        <div class="absolute rounded bg-gray-100 px-2 py-1 text-sm text-gray-900"
-                                            x-show="showTooltip">
-                                            <span>Ubah</span>
-                                        </div>
-                                    </div>
+                            <div x-data="{ showTooltip: false, isOpen: false }">
+                                <button class="text-gray-600 hover:text-red-600" type="button"
+                                    @mouseenter="showTooltip = true" @mouseleave="showTooltip = false"
+                                    @click="isOpen = true">
+                                    <x-atoms.svg.trash />
+                                </button>
 
-                                    <div x-data="{ isOpen: false }">
-                                        <button class="text-red-600 focus:outline-none" type="button"
-                                            @click="isOpen = true">
-                                            <x-atoms.svg.trash />
-                                        </button>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Delete</span>
+                                </div>
 
-                                        <x-molecules.modal-delete :title="'Apakah Anda akan yakin ingin menghapus nama akun : ' .
-                                            $item->fullname .
-                                            '?'" :action="route('kelolaAkun.destroy', $item->id)" />
-                                    </div>
-                                @endcan
-                            </td>
-                        </tr>
-                    </tbody>
-                @endforeach
-            @else
-                <tbody>
-                    <tr class="border-b bg-white">
-                        <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
-                            Data belum ada.
-                        </td>
-                    </tr>
-                </tbody>
-            @endif
-        </table>
-    </div>
+                                <x-molecules.modal.modal-delete :title="'Apakah Anda yakin ingin menghapus nama akun: ' . $item->fullname . '?'" :action="route('kelolaAkun.destroy', $item->id)" />
+                            </div>
+                        @endcan
+                    </td>
+                </tr>
+            @empty
+                <tr class="border-b bg-white">
+                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
+                        Data belum ada.
+                    </td>
+                </tr>
+            @endforelse
+        </x-molecules.table.table>
 
-    <div class="bg-white p-6">
-        {{ $user->links('vendor.pagination.tailwind') }}
+        <div class="border-t border-gray-200 px-6 py-4 dark:border-gray-800">
+            {{ $user->links('vendor.pagination.tailwind') }}
+        </div>
     </div>
 
 </x-app-dashboard>
