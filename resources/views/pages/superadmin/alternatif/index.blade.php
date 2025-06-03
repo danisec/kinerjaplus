3b3f1a83a48b5810ef018bef371a752d59913bcb
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:12:16 +0700

    refactor(alternatif): enhance layout and improve accessibility for employee list page

--- resources/views/pages/superadmin/alternatif/index.blade.php
+++ resources/views/pages/superadmin/alternatif/index.blade.php
@@ -1,118 +1,114 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Data Pegawai</span>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Pegawai</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
     <div class="my-8">
-        <h4 class="mb-6 text-2xl font-semibold text-gray-900">Data Pegawai</h4>
-
-        <div class="flex flex-row items-center justify-between">
-            <div>
-                <x-molecules.search :placeholder="'Cari Pegawai'" :request="request('nama_alternatif')" :name="'nama_alternatif'" :value="request('nama_alternatif')" />
-            </div>
-
+        <div class="flex flex-row items-center justify-end">
             @can('direct.permission', 'kelola pegawai')
                 <div>
                     <a href="{{ route('alternatif.create') }}">
-                        <x-atoms.button.button-primary :customClass="'h-12 w-40 rounded-md'" :type="'button'" :name="'Tambah Pegawai'" />
+                        <x-atoms.button.button-primary :type="'button'" :name="'Tambah Pegawai'" />
                     </a>
                 </div>
             @endcan
         </div>
     </div>
 
-    <div class="relative overflow-x-auto rounded-lg shadow-sm">
-        <table class="w-full text-left text-base text-gray-900">
-            <thead class="bg-slate-100 text-sm uppercase text-gray-900">
-                <tr>
-                    <th class="px-6 py-3" scope="col">
-                        Kode Alternatif
+    <div
+        class="overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 sm:px-6 dark:border-gray-800 dark:bg-white/[0.03]">
+        <x-molecules.table.table :title="'Daftar Pegawai'" :placeholderSearch="'Cari Pegawai'" :request="request('nama_alternatif')" :nameSearch="'nama_alternatif'">
+            <x-slot:thead>
+                <tr class="border-y border-gray-100 dark:border-gray-800">
+                    <th class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Nama Pegawai</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Nama Pegawai
-                    </th>
-                    <th class="flex justify-center px-6 py-3" scope="col">
-                        Aksi
+                    <th class="flex justify-center px-6 py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md text-center font-medium text-gray-800">Aksi
+                            </p>
+                        </div>
                     </th>
                 </tr>
-            </thead>
+            </x-slot:thead>
 
-            @if ($alternatif != null && $alternatif->count() > 0)
-                @foreach ($alternatif as $item)
-                    <tbody>
-                        <tr class="border-b bg-white hover:bg-slate-100">
-                            <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                                {{ $item->kode_alternatif }}
-                            </th>
-                            <td class="px-6 py-4">
+            @forelse ($alternatif as $item)
+                <tr>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-sm sm:text-theme-md text-gray-700 dark:text-gray-400">
                                 {{ $item->nama_alternatif }}
-                            </td>
-                            <td class="flex justify-center gap-4 px-6 py-4">
-                                @if (Auth::user()->hasAnyDirectPermission(['view pegawai', 'kelola pegawai']) ||
-                                        Auth::user()->canany(['view pegawai', 'kelola pegawai']))
-                                    <div x-data="{ showTooltip: false }">
-                                        <a class="font-medium text-gray-600"
-                                            href="{{ route('alternatif.show', $item->id_alternatif) }}"
-                                            @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                            <x-atoms.svg.eye />
-                                        </a>
+                            </p>
+                        </div>
+                    </td>
+                    <td class="xs:gap-2.5 flex justify-center p-6 sm:gap-4">
+                        @if (Auth::user()->hasAnyDirectPermission(['view pegawai', 'kelola pegawai']) ||
+                                Auth::user()->canany(['view pegawai', 'kelola pegawai']))
+                            <div x-data="{ showTooltip: false }">
+                                <a class="hover:text-blue-light-500 font-medium text-gray-600"
+                                    href="{{ route('alternatif.show', $item->id_alternatif) }}"
+                                    @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+                                    <x-atoms.svg.eye />
+                                </a>
 
-                                        <div class="absolute rounded bg-gray-100 px-2 py-1 text-sm text-gray-900"
-                                            x-show="showTooltip">
-                                            Detail
-                                        </div>
-                                    </div>
-                                @endif
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Show</span>
+                                </div>
+                            </div>
+                        @endif
 
-                                @can('direct.permission', 'kelola pegawai')
-                                    <div x-data="{ showTooltip: false }">
-                                        <a class="font-medium text-blue-600"
-                                            href="{{ route('alternatif.edit', $item->id_alternatif) }}"
-                                            @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                            <x-atoms.svg.pen />
-                                        </a>
+                        @can('direct.permission', 'kelola pegawai')
+                            <div x-data="{ showTooltip: false }">
+                                <a class="font-medium text-gray-600 hover:text-blue-600"
+                                    href="{{ route('alternatif.edit', $item->id_alternatif) }}"
+                                    @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+                                    <x-atoms.svg.pen />
+                                </a>
 
-                                        <div class="absolute rounded bg-gray-100 px-2 py-1 text-sm text-gray-900"
-                                            x-show="showTooltip">
-                                            <span>Ubah</span>
-                                        </div>
-                                    </div>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Edit</span>
+                                </div>
+                            </div>
 
-                                    <div x-data="{ isOpen: false }">
-                                        <button class="text-red-600 focus:outline-none" type="button"
-                                            @click="isOpen = true">
-                                            <x-atoms.svg.trash />
-                                        </button>
+                            <div x-data="{ showTooltip: false, isOpen: false }">
+                                <button class="text-gray-600 hover:text-red-600" type="button"
+                                    @mouseenter="showTooltip = true" @mouseleave="showTooltip = false"
+                                    @click="isOpen = true">
+                                    <x-atoms.svg.trash />
+                                </button>
 
-                                        <x-molecules.modal-delete :title="'Apakah Anda akan yakin ingin menghapus nama pegawai : ' .
-                                            $item->nama_alternatif .
-                                            ' ?'" :action="route('alternatif.destroy', $item->id_alternatif)" />
-                                    </div>
-                                @endcan
-                            </td>
-                        </tr>
-                    </tbody>
-                @endforeach
-            @else
-                <tbody>
-                    <tr class="border-b bg-white">
-                        <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
-                            Data belum ada.
-                        </td>
-                    </tr>
-                </tbody>
-            @endif
-        </table>
-    </div>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Delete</span>
+                                </div>
 
-    <div class="bg-white p-6">
-        {{ $alternatif->links('vendor.pagination.tailwind') }}
+                                <x-molecules.modal.modal-delete :title="'Apakah Anda yakin ingin menghapus nama pegawai: ' .
+                                    $item->nama_alternatif .
+                                    '?'" :action="route('alternatif.destroy', $item->id_alternatif)" />
+                            </div>
+                        @endcan
+                    </td>
+                </tr>
+            @empty
+                <tr class="border-b bg-white">
+                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
+                        Data belum ada.
+                    </td>
+                </tr>
+            @endforelse
+        </x-molecules.table.table>
+
+        <div class="border-t border-gray-200 px-6 py-4 dark:border-gray-800">
+            {{ $alternatif->links('vendor.pagination.tailwind') }}
+        </div>
     </div>
 
 </x-app-dashboard>
