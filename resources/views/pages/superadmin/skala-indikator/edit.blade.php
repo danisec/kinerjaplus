1579f046a108f770f5ae75c21a59aafc61d84e3a
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:14:41 +0700

    refactor(edit): enhance layout and improve accessibility for skala indikator edit page

--- resources/views/pages/superadmin/skala-indikator/edit.blade.php
+++ resources/views/pages/superadmin/skala-indikator/edit.blade.php
@@ -1,75 +1,79 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <a class="ml-1 text-base font-medium text-gray-900 hover:text-blue-600"
-                    href="{{ route('skalaIndikator.index') }}">Data Skala Indikator</a>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <a class="hover:text-brand-500 dark:hover:text-brand-400 xs:text-xs flex items-center gap-1 text-gray-500 sm:text-sm dark:text-gray-400"
+                href="{{ route('skalaIndikator.index') }}">Skala Indikator</a>
         </li>
 
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Ubah Skala Indikator</span>
-            </div>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Ubah Skala Indikator</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
-    <form class="mx-auto my-8 w-8/12" action="{{ route('skalaIndikator.update', $skalaIndikator->id_skala_indikator) }}"
-        method="POST">
+    <form class="my-8" action="{{ route('skalaIndikator.update', $skalaIndikator->id_skala_indikator) }}" method="POST">
         @method('PUT')
         @csrf
+        <div class="space-y-6">
+            <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                <div class="px-5 py-4 sm:px-6 sm:py-5">
+                    <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                        Ubah Skala Indikator
+                    </h3>
+                </div>
 
-        <div class="mt-8 space-y-6">
-            <h4 class="mb-6 text-2xl font-semibold text-gray-900">Ubah Skala Indikator</h4>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="nama subkriteria">
-                    Nama Subkriteria</label>
-
-                <input class="field-input-slate w-full" name="id_indikator_subkriteria" type="text"
-                    value="{{ $skalaIndikator->indikatorSubkriteria->subkriteria->kriteria->nama_kriteria . ' — ' . $skalaIndikator->indikatorSubkriteria->subkriteria->nama_subkriteria }}"
-                    @disabled(true) @readonly(true)>
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="indikator subkriteria">
-                    Indikator Subkriteria</label>
-
-                <textarea class="textAreaHeight field-input-slate w-full" name="id_indikator_subkriteria" rows="auto"
-                    @disabled(true) @readonly(true)>{{ $skalaIndikator->indikatorSubkriteria->indikator_subkriteria }}</textarea>
-            </div>
-
-            <div>
-                @foreach ($skalaIndikator->skalaIndikatorDetail as $item)
-                    <label class="mb-2 block text-base font-medium text-gray-900" for="skala indikator">
-                        Skala {{ $item->skala }}</label>
-
-                    <div class="kolom-skala-indikator my-4">
-                        <input name="skala[]" type="hidden" value="{{ $item->skala }}">
-                        <textarea class="textAreaHeight field-input-slate w-full" name="deskripsi_skala[]" rows="auto" required autofocus>{{ $item->deskripsi_skala }}</textarea>
-
-                        @error('deskripsi_skala')
-                            <p class="invalid-feedback">
-                                {{ $message }}
-                            </p>
-                        @enderror
+                <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
+                    <div>
+                        <x-molecules.input.input name="id_indikator_subkriteria" label="Nama Subkriteria"
+                            :type="'text'" :value="$skalaIndikator->indikatorSubkriteria->subkriteria->kriteria->nama_kriteria .
+                                ' — ' .
+                                $skalaIndikator->indikatorSubkriteria->subkriteria->nama_subkriteria" readonly disabled />
                     </div>
-                @endforeach
+
+                    <div>
+                        <x-molecules.textarea.textarea name="id_indikator_subkriteria" label="Indikator Subkriteria"
+                            :type="'text'" :value="$skalaIndikator->indikatorSubkriteria->indikator_subkriteria" rows="auto" readonly disabled />
+                    </div>
+                </div>
             </div>
-
         </div>
 
+        <div class="my-8">
+            <div class="space-y-6">
+                <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                    <div class="px-5 py-4 sm:px-6 sm:py-5">
+                        <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                            Skala Indikator
+                        </h3>
+                    </div>
 
-        <div class="flex flex-row gap-4">
-            <a href="{{ route('skalaIndikator.index') }}">
-                <x-atoms.button.button-gray :customClass="'w-52 text-center rounded-lg px-5 py-3'" :type="'button'" :name="'Kembali'" />
-            </a>
-            <x-atoms.button.button-primary :customClass="'w-full text-center rounded-lg px-5 py-3'" :type="'submit'" :name="'Ubah'" />
+                    <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
+                        @foreach ($skalaIndikator->skalaIndikatorDetail as $item)
+                            <div class="space-y-6">
+                                <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400"
+                                    for="skala indikator">
+                                    Skala {{ $item->skala }}</label>
+                                <div id="kolom-skala-indikator">
+                                    <input name="skala[]" type="hidden" value="{{ $item->skala }}">
+                                    <textarea
+                                        class="textAreaHeight dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:ring-brand-500/10 dark:focus:border-brand-800 focus:ring-3 focus:outline-hidden w-full rounded-lg border border-gray-300 bg-transparent px-4 py-2.5 text-sm text-gray-800 placeholder:text-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+                                        name="deskripsi_skala[]" rows="auto" required>{{ $item->deskripsi_skala }}</textarea>
+                                </div>
+                            </div>
+                        @endforeach
+
+                        <div class="flex flex-row justify-center gap-4">
+                            <a href="{{ route('skalaIndikator.index') }}">
+                                <x-atoms.button.button-secondary :type="'button'" :name="'Kembali'" />
+                            </a>
+                            <x-atoms.button.button-primary :type="'submit'" :name="'Ubah'" />
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </form>
 
 </x-app-dashboard>
