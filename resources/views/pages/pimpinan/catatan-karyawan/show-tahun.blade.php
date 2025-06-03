ec788eccf36b7061b1429ab39295df58e82c64a1
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:10:29 +0700

    refactor(show-tahun): enhance layout and improve accessibility for employee performance notes page

--- resources/views/pages/pimpinan/catatan-karyawan/show-tahun.blade.php
+++ resources/views/pages/pimpinan/catatan-karyawan/show-tahun.blade.php
@@ -1,153 +1,152 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <a class="ml-1 text-base font-medium text-gray-900 hover:text-blue-600"
-                    href="{{ route('catatanKaryawan.index') }}">Catatan Pegawai</a>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <a class="hover:text-brand-500 dark:hover:text-brand-400 xs:text-xs flex items-center gap-1 text-gray-500 sm:text-sm dark:text-gray-400"
+                href="{{ route('catatanKaryawan.index') }}">Catatan Pegawai</a>
         </li>
 
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium capitalize text-gray-500">
-                    {{ $tahunAjaranBreadcrumbs['name_group_karyawan'] . ' Tahun Ajaran ' . $tahunAjaranBreadcrumbs['tahun_ajaran'] . ' - Semester ' . $tahunAjaranBreadcrumbs['semester'] }}</span>
-            </div>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span
+                class="capitalize">{{ $tahunAjaranBreadcrumbs['name_group_karyawan'] . ' Tahun Ajaran ' . $tahunAjaranBreadcrumbs['tahun_ajaran'] . ' - Semester ' . $tahunAjaranBreadcrumbs['semester'] }}</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
-    <div class="my-8">
-        <h4 class="mb-6 text-2xl font-semibold capitalize text-gray-900">Catatan Pegawai Evaluasi Kinerja
-            {{ $tahunAjaranBreadcrumbs['name_group_karyawan'] . ' ' . $tahunAjaranBreadcrumbs['tahun_ajaran'] . ' - Semester ' . $tahunAjaranBreadcrumbs['semester'] }}
-        </h4>
-
-        <div class="flex flex-row items-center justify-between">
-            <div>
-                <x-molecules.search :placeholder="'Cari Nama Pegawai'" :request="request('nama_alternatif')" :name="'nama_alternatif'" :value="request('nama_alternatif')" />
-            </div>
-        </div>
-    </div>
-
-    <div class="relative overflow-x-auto rounded-lg shadow-sm">
-        <table class="w-full text-left text-base text-gray-900">
-            <thead class="bg-slate-100 text-sm uppercase text-gray-900">
-                <tr>
-                    <th class="px-6 py-3" scope="col">
-                        No.
+    <div
+        class="my-8 overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 sm:px-6 dark:border-gray-800 dark:bg-white/[0.03]">
+        <x-molecules.table.table
+            title="Catatan Pegawai Evaluasi Kinerja
+            {{ $tahunAjaranBreadcrumbs['tahun_ajaran'] . ' - Semester ' . $tahunAjaranBreadcrumbs['semester'] }}"
+            :placeholderSearch="'Cari Nama Pegawai'" :request="request('nama_alternatif')" :nameSearch="'nama_alternatif'">
+            <x-slot:thead>
+                <tr class="border-y border-gray-100 dark:border-gray-800">
+                    <th class="min-w-48 whitespace-nowrap py-3 text-left">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Nama Pemberi Nilai</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Tahun Ajaran
+                    <th class="min-w-48 whitespace-nowrap py-3 text-left">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Kepada</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Semester
+                    <th class="whitespace-nowrap py-3 text-left">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Penilaian</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Nama Pemberi Nilai
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        Kepada
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        Penilaian
-                    </th>
-                    <th class="px-6 py-3 text-center" scope="colgroup">
-                        Aksi
+                    <th class="flex justify-center px-6 py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md text-center font-medium text-gray-800">Aksi
+                            </p>
+                        </div>
                     </th>
                 </tr>
-            </thead>
+            </x-slot:thead>
 
-            @if ($catatanKaryawan != null && $catatanKaryawan->count() > 0)
-                @foreach ($catatanKaryawan as $item)
-                    <tbody>
-                        <tr class="border-b bg-white hover:bg-slate-100">
-                            <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                                {{ ($catatanKaryawan->currentPage() - 1) * $catatanKaryawan->perPage() + $loop->iteration }}
-                            </th>
-                            <td class="whitespace-nowrap px-6 py-4">
-                                {{ $item->tanggalPenilaian->tahun_ajaran }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4 capitalize">
-                                {{ $item->tanggalPenilaian->semester }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4">
+            @forelse ($catatanKaryawan as $item)
+                <tr>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-sm sm:text-theme-md text-gray-700 dark:text-gray-400">
                                 {{ $item->penilaian->alternatifPertama->alternatifPertama->nama_alternatif }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4">
+                            </p>
+                        </div>
+                    </td>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-sm sm:text-theme-md capitalize text-gray-700 dark:text-gray-400">
                                 {{ $item->penilaian->alternatifKedua->alternatifPertama->nama_alternatif }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4">
-                                <div x-data="{ showTooltip: false }">
-                                    <a class="font-medium text-blue-600 underline hover:text-blue-500"
-                                        href="{{ route('persetujuanPenilaian.show', $item->id_penilaian) }}"
-                                        target="_blank" rel="noreferrer noopener" @mouseenter="showTooltip = true"
-                                        @mouseleave="showTooltip = false">
-                                        Lihat Penilaian
-                                    </a>
-                                </div>
-                            </td>
-                            <td class="flex justify-center gap-4 px-6 py-4">
-                                <div x-data="{ showTooltip: false }">
-                                    <a class="font-medium text-gray-600"
-                                        href="{{ route('catatanKaryawan.show', ['id' => $item->id_catatan_karyawan, 'firstYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 0, 4), 'secondYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 5), 'semester' => $tahunAjaranBreadcrumbs['semester']]) }}"
-                                        @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                        <x-atoms.svg.eye />
-                                    </a>
+                            </p>
+                        </div>
+                    </td>
+                    <td class="py-3">
+                        <div class="flex items-center">
+                            <div x-data="{ showTooltip: false }">
+                                <a class="xs:text-xs font-medium text-blue-600 underline hover:text-blue-500 sm:text-sm"
+                                    href="{{ route('persetujuanPenilaian.show', $item->id_penilaian) }}" target="_blank"
+                                    rel="noreferrer noopener" @mouseenter="showTooltip = true"
+                                    @mouseleave="showTooltip = false">
+                                    Penilaian
+                                </a>
+                            </div>
+                        </div>
+                    </td>
+                    <td class="xs:gap-2.5 flex justify-center p-6 sm:gap-4">
+                        <div x-data="{ showTooltip: false }">
+                            <a class="hover:text-blue-light-500 font-medium text-gray-600"
+                                href="{{ route('catatanKaryawan.show', [
+                                    'id' => $item->id_catatan_karyawan,
+                                    'firstYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 0, 4),
+                                    'secondYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 5),
+                                    'semester' => $tahunAjaranBreadcrumbs['semester'],
+                                ]) }}"
+                                @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+                                <x-atoms.svg.eye />
+                            </a>
 
-                                    <div class="absolute rounded bg-gray-100 px-2 py-1 text-xs text-gray-900"
-                                        x-show="showTooltip">
-                                        Detail
-                                    </div>
-                                </div>
+                            <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                x-show="showTooltip" x-transition>
+                                <span>Show</span>
+                            </div>
+                        </div>
 
-                                <div x-data="{ showTooltip: false }">
-                                    <a class="font-medium text-blue-600"
-                                        href="{{ route('catatanKaryawan.edit', ['id' => $item->id_catatan_karyawan, 'firstYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 0, 4), 'secondYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 5), 'semester' => $tahunAjaranBreadcrumbs['semester']]) }}"
-                                        @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                        <x-atoms.svg.pen />
-                                    </a>
+                        <div x-data="{ showTooltip: false }">
+                            <a class="font-medium text-blue-600 hover:text-blue-600"
+                                href="{{ route('catatanKaryawan.edit', [
+                                    'id' => $item->id_catatan_karyawan,
+                                    'firstYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 0, 4),
+                                    'secondYear' => substr($tahunAjaranBreadcrumbs['tahun_ajaran'], 5),
+                                    'semester' => $tahunAjaranBreadcrumbs['semester'],
+                                ]) }}"
+                                @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+                                <x-atoms.svg.comment />
+                            </a>
 
-                                    <div class="absolute rounded bg-gray-100 px-2 py-1 text-sm text-gray-900"
-                                        x-show="showTooltip">
-                                        <span>Ubah</span>
-                                    </div>
-                                </div>
+                            <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                x-show="showTooltip" x-transition>
+                                <span>Edit</span>
+                            </div>
+                        </div>
 
-                                <div x-data="{ isOpen: false }">
-                                    <button class="text-red-600 focus:outline-none" type="button"
-                                        @click="isOpen = true">
-                                        <x-atoms.svg.trash />
-                                    </button>
+                        <div x-data="{ showTooltip: false, isOpen: false }">
+                            <button class="text-gray-600 hover:text-red-600" type="button"
+                                @mouseenter="showTooltip = true" @mouseleave="showTooltip = false"
+                                @click="isOpen = true">
+                                <x-atoms.svg.trash />
+                            </button>
 
-                                    <x-molecules.modal-delete :title="'Apakah Anda akan yakin ingin menghapus catatan pegawai : ' .
-                                        $item->penilaian->alternatifPertama->alternatifPertama->nama_alternatif .
-                                        ' Kepada ' .
-                                        $item->penilaian->alternatifKedua->alternatifPertama->nama_alternatif .
-                                        ' Tahun Ajaran ' .
-                                        $item->tanggalPenilaian->tahun_ajaran .
-                                        ' - Semester ' .
-                                        ucfirst(strtolower($item->tanggalPenilaian->semester)) .
-                                        '?'" :action="route('catatanKaryawan.destroy', $item->id_catatan_karyawan)" />
-                                </div>
-                            </td>
-                        </tr>
-                    </tbody>
-                @endforeach
-            @else
-                <tbody>
-                    <tr class="border-b bg-white">
-                        <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="6">
-                            Data belum ada.
-                        </td>
-                    </tr>
-                </tbody>
-            @endif
-        </table>
-    </div>
+                            <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                x-show="showTooltip" x-transition>
+                                <span>Delete</span>
+                            </div>
 
-    <div class="bg-white p-6">
-        {{ $catatanKaryawan->links('vendor.pagination.tailwind') }}
+                            <x-molecules.modal.modal-delete :title="'Apakah Anda yakin ingin menghapus catatan pegawai: ' .
+                                $item->penilaian->alternatifPertama->alternatifPertama->nama_alternatif .
+                                ' Kepada ' .
+                                $item->penilaian->alternatifKedua->alternatifPertama->nama_alternatif .
+                                ' Tahun Ajaran ' .
+                                $item->tanggalPenilaian->tahun_ajaran .
+                                ' - Semester ' .
+                                ucfirst(strtolower($item->tanggalPenilaian->semester)) .
+                                '?'" :action="route('catatanKaryawan.destroy', $item->id_catatan_karyawan)" />
+                        </div>
+                    </td>
+                </tr>
+            @empty
+                <tr class="border-b bg-white">
+                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
+                        Data belum ada.
+                    </td>
+                </tr>
+            @endforelse
+        </x-molecules.table.table>
+
+        <div class="border-t border-gray-200 px-6 py-4 dark:border-gray-800">
+            {{ $catatanKaryawan->links('vendor.pagination.tailwind') }}
+        </div>
     </div>
 
 </x-app-dashboard>
