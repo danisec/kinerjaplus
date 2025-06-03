a7cd809f5aaf0c40c062c5d441097ab749e38fda
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:07:39 +0700

    refactor(catatan-karyawan): enhance breadcrumb and table layout for improved readability and user experience

--- resources/views/pages/kepala-sekolah/catatan-karyawan/index.blade.php
+++ resources/views/pages/kepala-sekolah/catatan-karyawan/index.blade.php
@@ -1,93 +1,85 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Catatan Pegawai</span>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Catatan Pegawai</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
-    <div class="my-8">
-        <h4 class="mb-6 text-2xl font-semibold text-gray-900">Catatan Pegawai Evaluasi Kinerja</h4>
-
-        <div class="flex flex-row items-center justify-between">
-            <div>
-                <x-molecules.search :placeholder="'Cari Tahun Catatan Pegawai'" :request="request('nama_alternatif')" :name="'nama_alternatif'" :value="request('nama_alternatif')" />
-            </div>
-        </div>
-    </div>
-
-    <div class="relative overflow-x-auto rounded-lg shadow-sm">
-        <table class="w-full text-left text-base text-gray-900">
-            <thead class="bg-slate-100 text-sm uppercase text-gray-900">
-                <tr>
-                    <th class="px-6 py-3" scope="col">
-                        No.
+    <div
+        class="my-8 overflow-hidden rounded-2xl border border-gray-200 bg-white px-4 pb-3 pt-4 sm:px-6 dark:border-gray-800 dark:bg-white/[0.03]">
+        <x-molecules.table.table :title="'Daftar Catatan Pegawai'" :placeholderSearch="'Cari Catatan Pegawai'" :request="request('tahun_ajaran')" :nameSearch="'tahun_ajaran'">
+            <x-slot:thead>
+                <tr class="border-y border-gray-100 dark:border-gray-800">
+                    <th class="min-w-48 whitespace-nowrap py-3 text-left">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Nama Group Pegawai</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Tahun Ajaran
+                    <th class="whitespace-nowrap py-3 text-left">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md font-medium text-gray-800">Tahun Ajaran</p>
+                        </div>
                     </th>
-                    <th class="px-6 py-3" scope="col">
-                        Semester
-                    </th>
-                    <th class="px-6 py-3" scope="col">
-                        Nama Group Pegawai
-                    </th>
-                    <th class="flex justify-center px-6 py-3" scope="col">
-                        Aksi
+                    <th class="flex justify-center px-6 py-3">
+                        <div class="flex items-center">
+                            <p class="xs:text-xs sm:text-theme-md text-center font-medium text-gray-800">Aksi
+                            </p>
+                        </div>
                     </th>
                 </tr>
-            </thead>
+            </x-slot:thead>
 
-            <tbody>
-                @if ($catatanKaryawan != null && $catatanKaryawan->count() > 0)
-                    @foreach ($catatanWithGroupKaryawan as $index => $item)
-                        <tr class="border-b bg-white hover:bg-slate-100">
-                            <th class="whitespace-nowrap px-6 py-4 font-medium text-gray-900" scope="row">
-                                {{ $index + 1 }}
-                            </th>
-                            <td class="whitespace-nowrap px-6 py-4">
-                                {{ $item['tahun'] }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4 capitalize">
-                                {{ $item['semester'] }}
-                            </td>
-                            <td class="whitespace-nowrap px-6 py-4">
-                                {{ $item['namaGroupKaryawan'] }}
-                            </td>
-                            <td class="flex justify-center gap-4 px-6 py-4">
-                                <div x-data="{ showTooltip: false }">
-                                    <a class="font-medium text-gray-600"
-                                        href="{{ route('catatanKaryawan.showTahun', ['firstYear' => substr($item['tahun'], 0, 4), 'secondYear' => substr($item['tahun'], 5), 'semester' => $item['semester']]) }}"
-                                        @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
-                                        <x-atoms.svg.eye />
-                                    </a>
+            @if ($catatanKaryawan !== null)
+                @foreach ($catatanWithGroupKaryawan as $index => $item)
+                    <tr>
+                        <td class="py-3">
+                            <div class="flex items-center">
+                                <p class="xs:text-sm sm:text-theme-md text-gray-700 dark:text-gray-400">
+                                    {{ $item['namaGroupKaryawan'] }}
+                                </p>
+                            </div>
+                        </td>
+                        <td class="py-3">
+                            <div class="flex items-center">
+                                <p class="xs:text-sm sm:text-theme-md capitalize text-gray-700 dark:text-gray-400">
+                                    {{ $item['tahun'] }} - {{ $item['semester'] }}
+                                </p>
+                            </div>
+                        </td>
+                        <td class="xs:gap-2.5 flex justify-center p-6 sm:gap-4">
+                            <div x-data="{ showTooltip: false }">
+                                <a class="hover:text-blue-light-500 font-medium text-gray-600"
+                                    href="{{ route('catatanKaryawan.showTahun', [
+                                        'firstYear' => substr($item['tahun'], 0, 4),
+                                        'secondYear' => substr($item['tahun'], 5),
+                                        'semester' => $item['semester'],
+                                    ]) }}"
+                                    @mouseenter="showTooltip = true" @mouseleave="showTooltip = false">
+                                    <x-atoms.svg.eye />
+                                </a>
 
-                                    <div class="absolute rounded bg-gray-100 px-2 py-1 text-xs text-gray-900"
-                                        x-show="showTooltip">
-                                        Detail
-                                    </div>
+                                <div class="xs:text-xs absolute z-10 -ml-2.5 mt-1 rounded-sm bg-gray-100 px-2 py-1 text-gray-900 sm:text-sm"
+                                    x-show="showTooltip" x-transition>
+                                    <span>Show</span>
                                 </div>
-                            </td>
-                        </tr>
-                    @endforeach
-            </tbody>
-        @else
-            <tbody>
+                            </div>
+                        </td>
+                    </tr>
+                @endforeach
+            @else
                 <tr class="border-b bg-white">
-                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="6">
+                    <td class="px-6 py-4 text-center font-medium text-gray-600" colspan="3">
                         Data belum ada.
                     </td>
                 </tr>
-            </tbody>
             @endif
-        </table>
-    </div>
+        </x-molecules.table.table>
 
-    <div class="bg-white p-6">
-        {{ $catatanKaryawan->links('vendor.pagination.tailwind') }}
+        <div class="border-t border-gray-200 px-6 py-4 dark:border-gray-800">
+            {{ $catatanKaryawan->links('vendor.pagination.tailwind') }}
+        </div>
     </div>
 
 </x-app-dashboard>
