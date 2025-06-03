7d0166c35d1ae7249960bcc3fd3c61de131762ea
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:06:25 +0700

    refactor(create-akun): enhance breadcrumb structure and improve form layout for better readability and user experience

--- resources/views/pages/it/kelola-akun/create.blade.php
+++ resources/views/pages/it/kelola-akun/create.blade.php
@@ -1,117 +1,97 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <a class="ml-1 text-base font-medium text-gray-900 hover:text-blue-600"
-                    href="{{ route('kelolaAkun.index') }}">Kelola Akun</a>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <a class="hover:text-brand-500 dark:hover:text-brand-400 xs:text-xs flex items-center gap-1 text-gray-500 sm:text-sm dark:text-gray-400"
+                href="{{ route('kelolaAkun.index') }}">Kelola Akun</a>
         </li>
 
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Tambah Akun</span>
-            </div>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Tambah Akun</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
-    <div class="mx-auto my-8 w-8/12">
-        <h4 class="mb-6 text-2xl font-semibold text-gray-900">Tambah Akun</h4>
-
-        <form class="mt-8 space-y-6" action="{{ route('kelolaAkun.store') }}" method="POST">
-            @csrf
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="fullname">
-                    Nama Lengkap</label>
-                <input class="@error('fullname') border-red-500 @enderror field-input-slate w-full" name="fullname"
-                    type="text" value="{{ old('fullname') }}" autofocus placeholder="Nama Lengkap" required>
-
-                @error('fullname')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="username">
-                    Nama Pengguna</label>
-                <input class="@error('username') border-red-500 @enderror field-input-slate w-full" name="username"
-                    type="text" value="{{ old('username') }}" placeholder="Nama Pengguna" required>
-
-                @error('username')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="email">
-                    Email</label>
-                <input class="@error('email') border-red-500 @enderror field-input-slate w-full" name="email"
-                    type="email" value="{{ old('email') }}" placeholder="Email" required>
-
-                @error('email')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="role">
-                    Peran Pengguna</label>
-                <select class="@error('role') border-red-500 @enderror field-input-slate w-full" name="role"
-                    required>
-
-                    <option selected disabled hidden>Pilih Peran</option>
-                    @foreach ($roles as $role)
-                        <option class="capitalize" value="{{ $role->name }}"
-                            {{ old('role') == $role->name ? 'selected' : '' }}>
-                            {{ $role->name }}
-                        </option>
-                    @endforeach
-                </select>
-
-                @error('role')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="password">
-                    Password</label>
-
-                <div class="flex flex-row items-center justify-end">
-                    <input class="@error('password') border-red-500 @enderror field-input-slate w-full"
-                        id="passwordInput" name="password" type="password" value="{{ old('password') }}"
-                        placeholder="********" required>
-
-                    <button class="absolute mr-2.5" id="togglePasswordVisibility" type="button">
-                        <x-atoms.svg.eye id="eyeIcon" />
-                    </button>
+    <form class="my-8" action="{{ route('kelolaAkun.store') }}" method="POST">
+        @csrf
+        <div class="space-y-6">
+            <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                <div class="px-5 py-4 sm:px-6 sm:py-5">
+                    <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                        Tambah Akun
+                    </h3>
                 </div>
 
-                @error('password')
-                    <p class="invalid-feedback">
-                        {{ $message }}
-                    </p>
-                @enderror
-            </div>
+                <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
+                    <div>
+                        <x-molecules.input.input name="fullname" label="Nama Lengkap" :type="'text'"
+                            :value="old('fullname')" :placeholder="'Nama Lengkap'" required />
+                    </div>
 
-            <div class="flex flex-row gap-4">
-                <a href="{{ route('kelolaAkun.index') }}">
-                    <x-atoms.button.button-gray :customClass="'w-52 text-center rounded-lg px-5 py-3'" :type="'button'" :name="'Kembali'" />
-                </a>
-                <x-atoms.button.button-primary :customClass="'w-full text-center rounded-lg px-5 py-3'" :type="'submit'" :name="'Simpan'" />
+                    <div>
+                        <x-molecules.input.input name="username" label="Nama Pengguna" :type="'text'"
+                            :value="old('username')" :placeholder="'Nama Pengguna'" required />
+                    </div>
+
+                    <div>
+                        <x-molecules.input.input name="email" label="Email" :type="'email'" :value="old('email')"
+                            :placeholder="'example@mail.com'" required />
+                    </div>
+
+                    <div>
+                        <x-molecules.select.select name="role" label="Peran Pengguna" :options="$roles->mapWithKeys(fn($item) => [$item->name => $item->name])"
+                            :value="$roles" required />
+                    </div>
+
+                    <div id="copy-input">
+                        <label class="mb-1.5 block text-sm font-medium text-gray-700 dark:text-gray-400">
+                            Password
+                        </label>
+                        <div class="relative" x-data="{ showPassword: false }">
+                            <button
+                                class="absolute right-0 top-1/2 inline-flex -translate-y-1/2 cursor-pointer items-center gap-1 border-l border-gray-200 py-3 pl-3.5 pr-3 text-sm font-medium text-gray-700 dark:border-gray-800 dark:text-gray-400"
+                                id="copy-button">
+                                <svg class="fill-current" width="20" height="20" viewBox="0 0 20 20"
+                                    fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M6.58822 4.58398C6.58822 4.30784 6.81207 4.08398 7.08822 4.08398H15.4154C15.6915 4.08398 15.9154 4.30784 15.9154 4.58398L15.9154 12.9128C15.9154 13.189 15.6916 13.4128 15.4154 13.4128H7.08821C6.81207 13.4128 6.58822 13.189 6.58822 12.9128V4.58398ZM7.08822 2.58398C5.98365 2.58398 5.08822 3.47942 5.08822 4.58398V5.09416H4.58496C3.48039 5.09416 2.58496 5.98959 2.58496 7.09416V15.4161C2.58496 16.5207 3.48039 17.4161 4.58496 17.4161H12.9069C14.0115 17.4161 14.9069 16.5207 14.9069 15.4161L14.9069 14.9128H15.4154C16.52 14.9128 17.4154 14.0174 17.4154 12.9128L17.4154 4.58398C17.4154 3.47941 16.52 2.58398 15.4154 2.58398H7.08822ZM13.4069 14.9128H7.08821C5.98364 14.9128 5.08822 14.0174 5.08822 12.9128V6.59416H4.58496C4.30882 6.59416 4.08496 6.81801 4.08496 7.09416V15.4161C4.08496 15.6922 4.30882 15.9161 4.58496 15.9161H12.9069C13.183 15.9161 13.4069 15.6922 13.4069 15.4161L13.4069 14.9128Z"
+                                        fill="" />
+                                </svg>
+                                <div id="copy-text">Copy</div>
+                            </button>
+                            <input
+                                class="@error('password') border-red-500 @enderror dark:bg-dark-900 shadow-theme-xs focus:border-brand-300 focus:outline-hidden focus:ring-3 focus:ring-brand-500/10 dark:focus:border-brand-800 h-11 w-full rounded-lg border border-gray-300 bg-transparent py-2.5 pl-4 pr-11 text-sm text-gray-800 placeholder:text-gray-400 dark:border-gray-700 dark:bg-gray-900 dark:text-white/90 dark:placeholder:text-white/30"
+                                id="website-input" name="password" value="{{ $randomPassword }}"
+                                :type="showPassword ? 'text' : 'password'" readonly />
+                            <span
+                                class="absolute right-24 top-1/2 z-30 -translate-y-1/2 cursor-pointer text-gray-500 dark:text-gray-400"
+                                @click="showPassword = !showPassword">
+                                <svg class="fill-current" x-show="showPassword" width="20" height="20"
+                                    viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M10.0002 13.8619C7.23361 13.8619 4.86803 12.1372 3.92328 9.70241C4.86804 7.26761 7.23361 5.54297 10.0002 5.54297C12.7667 5.54297 15.1323 7.26762 16.0771 9.70243C15.1323 12.1372 12.7667 13.8619 10.0002 13.8619ZM10.0002 4.04297C6.48191 4.04297 3.49489 6.30917 2.4155 9.4593C2.3615 9.61687 2.3615 9.78794 2.41549 9.94552C3.49488 13.0957 6.48191 15.3619 10.0002 15.3619C13.5184 15.3619 16.5055 13.0957 17.5849 9.94555C17.6389 9.78797 17.6389 9.6169 17.5849 9.45932C16.5055 6.30919 13.5184 4.04297 10.0002 4.04297ZM9.99151 7.84413C8.96527 7.84413 8.13333 8.67606 8.13333 9.70231C8.13333 10.7286 8.96527 11.5605 9.99151 11.5605H10.0064C11.0326 11.5605 11.8646 10.7286 11.8646 9.70231C11.8646 8.67606 11.0326 7.84413 10.0064 7.84413H9.99151Z"
+                                        fill="#98A2B3" />
+                                </svg>
+                                <svg class="fill-current" x-show="!showPassword" width="20" height="20"
+                                    viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" clip-rule="evenodd"
+                                        d="M4.63803 3.57709C4.34513 3.2842 3.87026 3.2842 3.57737 3.57709C3.28447 3.86999 3.28447 4.34486 3.57737 4.63775L4.85323 5.91362C3.74609 6.84199 2.89363 8.06395 2.4155 9.45936C2.3615 9.61694 2.3615 9.78801 2.41549 9.94558C3.49488 13.0957 6.48191 15.3619 10.0002 15.3619C11.255 15.3619 12.4422 15.0737 13.4994 14.5598L15.3625 16.4229C15.6554 16.7158 16.1302 16.7158 16.4231 16.4229C16.716 16.13 16.716 15.6551 16.4231 15.3622L4.63803 3.57709ZM12.3608 13.4212L10.4475 11.5079C10.3061 11.5423 10.1584 11.5606 10.0064 11.5606H9.99151C8.96527 11.5606 8.13333 10.7286 8.13333 9.70237C8.13333 9.5461 8.15262 9.39434 8.18895 9.24933L5.91885 6.97923C5.03505 7.69015 4.34057 8.62704 3.92328 9.70247C4.86803 12.1373 7.23361 13.8619 10.0002 13.8619C10.8326 13.8619 11.6287 13.7058 12.3608 13.4212ZM16.0771 9.70249C15.7843 10.4569 15.3552 11.1432 14.8199 11.7311L15.8813 12.7925C16.6329 11.9813 17.2187 11.0143 17.5849 9.94561C17.6389 9.78803 17.6389 9.61696 17.5849 9.45938C16.5055 6.30925 13.5184 4.04303 10.0002 4.04303C9.13525 4.04303 8.30244 4.17999 7.52218 4.43338L8.75139 5.66259C9.1556 5.58413 9.57311 5.54303 10.0002 5.54303C12.7667 5.54303 15.1323 7.26768 16.0771 9.70249Z"
+                                        fill="#98A2B3" />
+                                </svg>
+                            </span>
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row justify-center gap-4">
+                        <a href="{{ route('kelolaAkun.index') }}">
+                            <x-atoms.button.button-secondary :type="'button'" :name="'Kembali'" />
+                        </a>
+                        <x-atoms.button.button-primary :type="'submit'" :name="'Simpan'" />
+                    </div>
+                </div>
             </div>
-        </form>
-    </div>
+        </div>
+    </form>
 
 </x-app-dashboard>
