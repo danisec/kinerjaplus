4f60c0bb44dbde19d5a940355aa0d0f20585c15a
danisec <daniaprilyanto35@gmail.com>
2025-06-03 09:14:09 +0700

    refactor(show): enhance layout and improve accessibility for kriteria detail page

--- resources/views/pages/superadmin/kriteria/show.blade.php
+++ resources/views/pages/superadmin/kriteria/show.blade.php
@@ -1,57 +1,57 @@
 <x-app-dashboard title="{{ $title }}">
 
-    <x-molecules.breadcrumb>
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <a class="ml-1 text-base font-medium text-gray-900 hover:text-blue-600"
-                    href="{{ route('kriteria.index') }}">Data Kriteria</a>
-            </div>
+    <x-molecules.breadcrumb.breadcrumb>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <a class="hover:text-brand-500 dark:hover:text-brand-400 xs:text-xs flex items-center gap-1 text-gray-500 sm:text-sm dark:text-gray-400"
+                href="{{ route('kriteria.index') }}">Kriteria</a>
         </li>
 
-        <li aria-current="page">
-            <div class="flex items-center">
-                <x-atoms.svg.arrow-right />
-                <span class="mx-2 text-base font-medium text-gray-500">Detail Kriteria</span>
-            </div>
+        <li class="xs:text-xs flex items-center gap-0.5 text-gray-800 sm:text-sm dark:text-white/90">
+            <x-atoms.svg.arrow-right />
+            <span>Detail Kriteria</span>
         </li>
-    </x-molecules.breadcrumb>
+    </x-molecules.breadcrumb.breadcrumb>
 
-    <div class="mx-auto my-8 w-8/12">
-        <h4 class="mb-6 text-2xl font-semibold text-gray-900">Detail Kriteria</h4>
-
-        <div class="mt-8 space-y-6">
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="kode kriteria">
-                    Kode Kriteria</label>
-                <input class="field-input-slate w-full" name="kode_kriteria" type="text"
-                    value="{{ $kriteria->kode_kriteria }}" @disabled(true) @readonly(true)>
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="nama kriteria">
-                    Nama Kriteria</label>
-                <input class="field-input-slate w-full" name="nama_kriteria" type="text"
-                    value="{{ $kriteria->nama_kriteria }}" @disabled(true) @readonly(true)>
-            </div>
-
-            <div>
-                <label class="mb-2 block text-base font-medium text-gray-900" for="bobot kriteria">
-                    Bobot Kriteria</label>
-
-                <div class="flex flex-row justify-between gap-4">
-                    <input class="field-input-slate w-full" name="bobot_kriteria" type="number"
-                        value="{{ $kriteria->bobot_kriteria }}" @disabled(true) @readonly(true)>
-
-                    <input class="field-input-slate w-10 text-center" type="text" value="%"
-                        @disabled(true) @readonly(true)>
+    <div class="my-8">
+        <div class="space-y-6">
+            <div class="rounded-2xl border border-gray-200 bg-white dark:border-gray-800 dark:bg-white/[0.03]">
+                <div class="px-5 py-4 sm:px-6 sm:py-5">
+                    <h3 class="text-base font-medium text-gray-800 dark:text-white/90">
+                        Detail Kriteria
+                    </h3>
                 </div>
-            </div>
 
-            <div class="flex justify-center">
-                <a href="{{ route('kriteria.index') }}">
-                    <x-atoms.button.button-gray :customClass="'w-52 text-center rounded-lg px-5 py-3'" :type="'button'" :name="'Kembali'" />
-                </a>
+                <div class="space-y-6 border-t border-gray-100 p-5 sm:p-6 dark:border-gray-800">
+                    <div>
+                        <x-molecules.input.input name="kode_kriteria" label="Kode Kriteria" :type="'text'"
+                            :value="$kriteria->kode_kriteria" readonly />
+                    </div>
+
+                    <div>
+                        <x-molecules.input.input name="nama_kriteria" label="Nama Kriteria" :type="'text'"
+                            :value="$kriteria->nama_kriteria" readonly />
+                    </div>
+
+                    <div class="flex flex-row items-center justify-between gap-4">
+                        <div class="w-full">
+                            <x-molecules.input.input name="bobot_kriteria" label="Bobot Kriteria" :type="'number'"
+                                min="1" maxlength="3" minlength="1" max="100" :value="$kriteria->bobot_kriteria"
+                                readonly />
+                        </div>
+
+                        <div class="mt-5 w-14">
+                            <x-molecules.input.input name="" label="" :type="'text'" :value="'%'"
+                                readonly />
+                        </div>
+                    </div>
+
+                    <div class="flex flex-row justify-center">
+                        <a href="{{ route('kriteria.index') }}">
+                            <x-atoms.button.button-secondary :type="'button'" :name="'Kembali'" />
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
